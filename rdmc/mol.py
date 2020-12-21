4c9947ba6ed12468908cc9e95eb6220162674e2d
Xiaorui Dong <xiaorui@mit.edu>
2020-12-21 17:27:49 -0500

    Enable importing from and exporting to OBMol

--- rdmc/mol.py
+++ rdmc/mol.py
@@ -8,13 +8,30 @@ This module provides class and methods for dealing with RDKit RWMol, Mol.
 from typing import Union
 
 from rdkit import Chem
-from rdkit.Chem.rdchem import Mol, RWMol
+from rdkit.Chem.rdchem import BondType, Mol, RWMol
 
+# openbabel import is currently put behind rdkit.
+# This relates to https://github.com/rdkit/rdkit/issues/2292
+# For Mac, with anaconda build:
+# rdkit -  2020.03.2.0 from rdkit channel
+# openbabel -  2.4.1 from rmg channel
+# works fine without sequence problem
+# For Linux,
+# rdkit - 2020.03.3 from rdkit channel
+# openbabel - 2.4.1 from rmg channel does not work
+import openbabel as ob
 
 # Keep the representation method from rdchem.Mol
 KEEP_RDMOL_ATTRIBUTES = ['_repr_html_',
                          '_repr_png_',
                          '_repr_svg_']
+
+# Bond order dictionary for RDKit, numbers are the bond order.
+# Note: There is a bond type 'OTHER' which may be helpful to treat TSs
+ORDERS = {1: BondType.SINGLE, 2: BondType.DOUBLE, 3: BondType.TRIPLE, 1.5: BondType.AROMATIC,
+          4: BondType.QUADRUPLE,
+          'S': BondType.SINGLE, 'D': BondType.DOUBLE, 'T': BondType.TRIPLE, 'B': BondType.AROMATIC,
+          'Q': BondType.QUADRUPLE}
 
 
 class RDKitMol(object):
@@ -54,6 +71,26 @@ class RDKitMol(object):
 
         # Perceive rings
         Chem.GetSymmSSSR(self._mol)
+
+    @ classmethod
+    def FromOBMol(cls,
+                  ob_mol: 'openbabel.OBMol',
+                  remove_h: bool = False,
+                  sanitize: bool = True,
+                  ) -> 'RDKitMol':
+        """
+        Convert a OpenBabel molecular structure to an RDKit RDMol object.
+
+        Args:
+            ob_mol (Molecule): An OpenBabel Molecule object for the conversion.
+            remove_h (bool, optional): Whether to remove hydrogen atoms from the molecule, Defaults to True.
+            sanitize (bool, optional): Whether to sanitize the RDKit molecule. Defaults to True.
+
+        Returns:
+            RDKitMol: An RDKit molecule object corresponding to the input OpenBabel Molecule object.
+        """
+        rw_mol = openbabel_mol_to_rdkit_mol(ob_mol, remove_h, sanitize)
+        return cls(rw_mol)
 
     @ classmethod
     def FromSmiles(cls,
@@ -147,3 +184,93 @@ class RDKitMol(object):
                                            isomericSmiles=stereo,
                                            kekuleSmiles=kekule,
                                            canonical=canonical)
+
+    def ToOBMol(self):
+        """
+        Convert RDKitMol to a OBMol.
+
+        Returns:
+            OBMol: The corresponding openbabel OBMol.
+        """
+        return rdkit_mol_to_openbabel_mol(self)
+
+
+def openbabel_mol_to_rdkit_mol(obmol: 'openbabel.OBMol',
+                               remove_h: bool = False,
+                               sanitize: bool = True,
+                               ) -> 'RDKitMol':
+    """
+    Convert a OpenBabel molecular structure to a Chem.rdchem.RWMol object.
+    Args:
+        ob_mol (Molecule): An OpenBabel Molecule object for the conversion.
+        remove_h (bool, optional): Whether to remove hydrogen atoms from the molecule, Defaults to True.
+        sanitize (bool, optional): Whether to sanitize the RDKit molecule. Defaults to True.
+
+    Returns:
+        RWMol: A writable RDKit RWMol instance.
+    """
+    rw_mol = Chem.rdchem.RWMol()
+    for obatom in ob.OBMolAtomIter(obmol):
+        atom = Chem.rdchem.Atom(obatom.GetAtomicNum())
+        isotope = obatom.GetIsotope()
+        if isotope != 0:
+            atom.SetIsotope(isotope)
+        spin = obatom.GetSpinMultiplicity()
+        if spin == 2:  # radical
+            atom.SetNumRadicalElectrons(1)
+        elif spin in [1, 3]:  # carbene
+            # TODO: Not sure if singlet and triplet are distinguished
+            atom.SetNumRadicalElectrons(2)
+        atom.SetFormalCharge(obatom.GetFormalCharge())
+        if not (remove_h and obatom.GetAtomicNum == 1):
+            rw_mol.AddAtom(atom)
+
+    for bond in ob.OBMolBondIter(obmol):
+        # Atom indexes in Openbael is 1-indexed, so we need to convert them to 0-indexed
+        atom1_idx = bond.GetBeginAtomIdx() - 1
+        atom2_idx = bond.GetEndAtomIdx() - 1
+        # Get the bond order. For aromatic molecules, the bond order is not
+        # 1.5 but 1 or 2. Manually set them to 1.5
+        bond_order = bond.GetBondOrder() if not bond.IsAromatic() else 1.5
+
+        rw_mol.AddBond(atom1_idx, atom2_idx, ORDERS[bond_order])
+
+    # Rectify the molecule
+    if remove_h:
+        rw_mol = Chem.RemoveHs(rw_mol, sanitize=sanitize)
+    elif sanitize:
+        Chem.SanitizeMol(rw_mol)
+    return rw_mol
+
+
+def rdkit_mol_to_openbabel_mol(rdmol: Union[Mol, RWMol]):
+    """
+    Convert a Mol/RWMol to a Openbabel mol.
+    Args:
+        rdmol: The RDKit Mol/RWMol object to be converted.
+
+    Returns:
+        OBMol: An openbabel OBMol instance.
+    """
+    obmol = ob.OBMol()
+    for rdatom in rdmol.GetAtoms():
+        obatom = obmol.NewAtom()
+        obatom.SetAtomicNum(rdatom.GetAtomicNum())
+        isotope = rdatom.GetIsotope()
+        if isotope != 0:
+            obatom.SetIsotope(isotope)
+        obatom.SetFormalCharge(rdatom.GetFormalCharge())
+    bond_type_dict = {BondType.SINGLE: 1,
+                      BondType.DOUBLE: 2,
+                      BondType.TRIPLE: 3,
+                      BondType.QUADRUPLE: 4,
+                      BondType.AROMATIC: 5}
+    for bond in rdmol.GetBonds():
+        atom1_idx = bond.GetBeginAtomIdx() + 1
+        atom2_idx = bond.GetEndAtomIdx() + 1
+        order = bond_type_dict[bond.GetBondType()]
+        obmol.AddBond(atom1_idx, atom2_idx, order)
+
+    obmol.AssignSpinMultiplicity(True)
+
+    return obmol
