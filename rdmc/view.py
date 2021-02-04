e6c3cc3ae5fdaccccc95e26a7120ae97c8dce027
Xiaorui Dong <xiaorui@mit.edu>
2021-02-03 20:53:24 -0500

    Add molecule 3D geometry viewer powered by py3Dmol

---
+++ rdmc/view.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+A module contains py3Dmol functions to draw molecules
+"""
+
+from typing import Optional
+
+import py3Dmol
+
+
+def mol_viewer(obj: str,
+               model: str = 'xyz',
+               model_extra: Optional[dict] = None,
+               animate: Optional[dict] = None,
+               atom_index: bool = True,
+               style_spec: Optional[dict] = None,
+               viewer: Optional[py3Dmol.view] = None,
+               viewer_size: tuple = (400, 400),
+               viewer_loc: Optional[tuple] = None,
+               ) -> py3Dmol.view:
+    """
+    This is the most general function to view a molecule powered by py3Dmol
+
+    Args:
+        obj (str): A string representation of the molecule can be xyz string,
+                   sdf string, etc.
+        model (str, optional): The model (format) of the molecule representation, e.g., ``'xyz'``.
+                     Defaults to ``'xyz'``.
+        model_extra (dict, optional): Extra specs for the model. E.g., frequency specs
+                     Can be provided here. Default to ``None``
+        animate (dict, optional): Specs for animation. E.g., ``{'loop': 'backAndForth'}``.
+        atom_index (bool, optional): Whether to show atom index. Defaults to True.
+        style_spec (dict, Optional): Style of the shown molecule. The default is showing both
+                                     both atoms and bonds.
+        viewer (py3Dmol.view, optional): Provide an existing viewer, instead of create a new one.
+        viewer_size (tuple, optional): Set the viewer size. Only useful if ``viewer`` is not provided.
+                                       Defaults to (400, 400).
+        viewer_loc (tuple, optional): The location of the viewer in the grid. E.g., (0, 1). Defaults to None.
+
+    Returns:
+        py3Dmol.view: The molecule viewer.
+    """
+    if not viewer:
+        viewer = py3Dmol.view(width=viewer_size[0], height=viewer_size[1])
+
+    if not model_extra:
+        viewer.addModel(obj, model, viewer=viewer_loc)
+    else:
+        viewer.addModel(obj, model, model_extra, viewer=viewer_loc)
+
+    if style_spec is None:
+        viewer.setStyle({'stick': {'radius': 0.2},
+                        'sphere': {'scale': 0.25},},)
+
+    else:
+        viewer.setStyle(style_spec, viewer=viewer_loc)
+
+    if animate:
+        viewer.animate(animate, viewer=viewer_loc)
+
+    if atom_index:
+        viewer.addPropertyLabels("index",
+                                 "",
+                                {'fontSize':15,
+                                  'fontColor':'white',
+                                  'showBackground': False,
+                                  'alignment': 'center',
+                                  'showBackground': True,
+                                  'backgroundOpacity': 0.2,
+                                  'backgroundColor': 'black',
+                                 }, viewer=viewer_loc)
+    viewer.zoomTo(viewer=viewer_loc)
+    return viewer
+
+def freq_viewer(obj: str,
+                model: str = 'xyz',
+                frames: int = 10,
+                amplitude: float = 1.,
+                atom_index: bool = True,
+                style_spec: Optional[dict] = None,
+                viewer: Optional[py3Dmol.view] = None,
+                viewer_size: tuple = (400, 400),
+                viewer_loc: Optional[tuple] = None,
+                ) -> py3Dmol.view:
+    """
+    This is the viewer for frequency.
+
+    Args:
+        obj (str): A string representation of the molecule can be xyz string,
+                   sdf string, etc.
+        model (str, optional): The model (format) of the molecule representation, e.g., ``'xyz'``.
+                     Defaults to ``'xyz'``.
+        frames (int, optional): Number of frames to be created.
+        amplitude (float, optional): amplitude of distortion.
+        atom_index (bool, optional): Whether to show atom index. Defaults to True.
+        style_spec (dict, Optional): Style of the shown molecule. The default is showing both
+                                     both atoms and bonds.
+        viewer (py3Dmol.view, optional): Provide an existing viewer, instead of create a new one.
+        viewer_size (tuple, optional): Set the viewer size. Only useful if ``viewer`` is not provided.
+                                       Defaults to (400, 400).
+        viewer_loc (tuple, optional): The location of the viewer in the grid. E.g., (0, 1). Defaults to None.
+
+    Returns:
+        py3Dmol.view: The molecule frequence viewer.
+    """
+    model_extra = {'vibrate': {'frames': frames,'amplitude': amplitude}},
+    animate = {'loop': 'backAndForth'}
+    return mol_viewer(obj, model, model_extra, animate, atom_index,
+                      style_spec, viewer, viewer_size, viewer_loc)
