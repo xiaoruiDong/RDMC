161369b66a479cd8bf6dd55cbd860e726319f54a
Xiaorui Dong <xiaorui@mit.edu>
2021-09-16 12:39:40 -0400

    Update the saturate mol to make it work for atomic oxygen
    Also add an example of 1,2 cycloaddition for O + C=C into the Match RMG reaction

--- rdmc/utils.py
+++ rdmc/utils.py
@@ -35,9 +35,9 @@ ROTATABLE_BOND_SMARTS_WO_METHYL = Chem.MolFromSmarts('[!$(*#*)&!D1!H3]-&!@[!$(*#
 CO_OPENBABEL_PATTERN = ob.OBSmartsPattern()
 CO_OPENBABEL_PATTERN.Init('[C;v2]=[O]')
 
-# Carbene and nitrene templates. RDKit and Openbabel have difficulty
+# Carbene, nitrene, and atomic oxygen templates. RDKit and Openbabel have difficulty
 # distinguish their multiplicity when input as SMILES or XYZ
-CARBENE_PATTERN = Chem.MolFromSmarts('[Cv0,Cv1,Cv2,Nv0,Nv1]')
+CARBENE_PATTERN = Chem.MolFromSmarts('[Cv0,Cv1,Cv2,Nv0,Nv1,Ov0]')
 
 def determine_smallest_atom_index_in_torsion(atom1: 'rdkit.Chem.rdchem.Atom',
                                              atom2: 'rdkit.Chem.rdchem.Atom',
