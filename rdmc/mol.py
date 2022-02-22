ef21f634dcb64d4e37a3be8f305e12820951c071
PattanaikL <lagnajit@mit.edu>
2022-02-22 10:58:26 -0500

    BugFix: change FromFile class method to use f.read() instead of f.readlines()

--- rdmc/mol.py
+++ rdmc/mol.py
@@ -580,7 +580,7 @@ class RDKitMol(object):
 
         if extension == ".xyz":
             with open(path, "r") as f:
-                xyz = f.readlines()
+                xyz = f.read()
             return cls.FromXYZ(xyz, batcken=backend, header=header, correctCO=correctCO, **kwargs)
 
         # use rdkit's sdf reader to read in multiple mols
