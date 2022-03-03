ae40eed6d4261335bf78856815e7db26b9587ac7
PattanaikL <lagnajit@mit.edu>
2022-03-03 10:29:37 -0500

    BugFix: correctly stop generation if only single conformer present by clipping metric

--- rdmc/conformer_generation/metrics.py
+++ rdmc/conformer_generation/metrics.py
@@ -37,7 +37,7 @@ class SCGMetric:
 
         min_metric = np.min(self.metric_history[-self.window:])
         max_metric = np.max(self.metric_history[-self.window:])
-        change = (max_metric - min_metric) / min_metric
+        change = (max_metric - min_metric) / np.clip(min_metric, a_min=1e-10, a_max=None)
         return True if change <= self.threshold else False
 
     def calculate_entropy(self, mol_data):
