df0a7d9901908e12e31aa697183bf7f350b64b67
PattanaikL <lagnajit@mit.edu>
2022-03-21 17:56:39 -0400

    added orca ts optimizer

--- rdmc/conformer_generation/ts_optimizers.py
+++ rdmc/conformer_generation/ts_optimizers.py
@@ -6,9 +6,12 @@ Modules for optimizing transition state geometries
 """
 
 from rdkit import Chem
+from rdmc import RDKitMol
 import os
 from time import time
 from rdmc.external.sella import run_sella_opt
+from rdmc.external.orca import write_orca_opt
+import subprocess
 
 
 class TSOptimizer:
@@ -67,6 +70,51 @@ class SellaOptimizer(TSOptimizer):
                                     steps=self.steps,
                                     save_dir=ts_conf_dir
                                     )
+        if save_dir:
+            self.save_opt_mols(save_dir, opt_mol.ToRWMol())
+
+        return opt_mol
+
+
+class OrcaOptimizer(TSOptimizer):
+    def __init__(self, method="XTB2", track_stats=False):
+        super(OrcaOptimizer, self).__init__(track_stats)
+
+        self.method = method
+
+    def save_opt_mols(self, save_dir, opt_mol):
+
+        # save optimized ts mols
+        ts_path = os.path.join(save_dir, "ts_optimized_confs.sdf")
+        with Chem.rdmolfiles.SDWriter(ts_path) as ts_writer:
+            [ts_writer.write(opt_mol, confId=i) for i in range(opt_mol.GetNumConformers())]
+
+    def optimize_ts_guesses(self, mol, save_dir=None):
+
+        ORCA_BINARY = os.environ.get("ORCA")
+        opt_mol = mol.Copy(quickCopy=True)
+        for i in range(mol.GetNumConformers()):
+            if save_dir:
+                ts_conf_dir = os.path.join(save_dir, f"orca_opt{i}")
+                if not os.path.exists(ts_conf_dir):
+                    os.makedirs(ts_conf_dir)
+
+            orca_str = write_orca_opt(mol, confId=i)
+            orca_input_file = os.path.join(ts_conf_dir, "orca_opt.inp")
+            with open(orca_input_file, "w") as f:
+                f.writelines(orca_str)
+
+            with open(os.path.join(ts_conf_dir, "orca_opt.log"), "w") as f:
+                orca_run = subprocess.run(
+                    [ORCA_BINARY, orca_input_file],
+                    stdout=f,
+                    stderr=subprocess.STDOUT,
+                    cwd=os.getcwd(),
+                )
+            if orca_run.returncode != 0:
+                mol = RDKitMol.FromFile(os.path.join(ts_conf_dir, "orca_opt.xyz"), sanitize=False)
+                opt_mol.AddConformer(mol.GetConformer().ToConformer(), assignId=True)
+                continue
         if save_dir:
             self.save_opt_mols(save_dir, opt_mol.ToRWMol())
 
--- rdmc/external/orca.py
+++ rdmc/external/orca.py
@@ -18,3 +18,22 @@ def write_orca_irc(mol, confId=0, maxcores=1000, nprocs=1):
     *
     """
     return orca_irc_input
+
+
+def write_orca_opt(mol, confId=0, maxcores=1000, nprocs=1, method="XTB2"):
+
+    orca_opt_input = f"""! {method} OptTS NumFreq
+    %maxcore {maxcores}
+    %pal
+    nprocs {nprocs}
+    end
+    %geom
+    Calc_Hess true # Calculate Hessian in the beginning
+    NumHess true  # Numerical hessian for semiempirical methods
+    Recalc_Hess 5  # Recalculate the Hessian every 5 steps
+    end
+    *xyz {mol.GetFormalCharge()} {mol.GetSpinMultiplicity()}
+    {mol.ToXYZ(header=False, confId=confId)}
+    *
+    """
+    return orca_opt_input
