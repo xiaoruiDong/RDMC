ede26b067a0a61d901e9c7426fcd26089ea7c6ce
PattanaikL <lagnajit@mit.edu>
2022-03-23 15:17:59 -0400

    allow get_mol method from GaussianLog class to forgo sanitization

--- rdmc/external/gaussian.py
+++ rdmc/external/gaussian.py
@@ -607,7 +607,8 @@ class GaussianLog(object):
                embed_conformers: bool = True,
                converged: bool = True,
                neglect_spin: bool = True,
-               backend: str = 'openbabel'
+               backend: str = 'openbabel',
+               sanitize: bool = False,
                ) -> 'RDKitMol':
         """
         Perceive the xyzs in the file and turn the geometries to conformers.
@@ -622,6 +623,7 @@ class GaussianLog(object):
                                  between the generated mol and the value in the output file. This
                                  can be useful for calculations involves TS. Defaults to ``True``.
             backend (str): The backend engine for parsing XYZ. Defaults to ``'openbabel'``.
+            sanitize (bool, optional): Whether to sanitize the RDKit molecule, ``True`` to sanitize.
 
         Returns:
             RDKitMol: a molecule generated from the output file.
@@ -641,7 +643,7 @@ class GaussianLog(object):
             # If not converged, use the initial guess geometry
             xyz = self.cclib_results.writexyz(indices=-self.num_all_geoms)
 
-        mol = RDKitMol.FromXYZ(xyz, backend=backend)
+        mol = RDKitMol.FromXYZ(xyz, backend=backend, sanitize=sanitize)
 
         if mol.GetSpinMultiplicity() != self.multiplicity:
             mol.SaturateMol(multiplicity=self.multiplicity)
