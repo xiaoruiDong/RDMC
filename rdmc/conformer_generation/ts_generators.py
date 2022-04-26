836c488793e9929a44bb19cd398710a0626d89bf
Lagnajit Pattanaik <lagnajit@mit.edu>
2022-04-26 17:43:24 -0400

    don't regenerate mol object after pruning step (since pruned conformers are tracked with the unique_ids list and the KeepIDs dict)

--- rdmc/conformer_generation/ts_generators.py
+++ rdmc/conformer_generation/ts_generators.py
@@ -237,10 +237,9 @@ class TSConformerGenerator:
 
         if self.pruner:
             self.logger.info("Pruning TS guesses...")
-            opt_ts_mol_dict, unique_ids = self.pruner(mol_to_dict(opt_ts_mol, conf_copy_attrs=["KeepIDs", "energy"]),
-                                                      sort_by_energy=False, return_ids=True)
+            _, unique_ids = self.pruner(mol_to_dict(opt_ts_mol, conf_copy_attrs=["KeepIDs", "energy"]),
+                                        sort_by_energy=False, return_ids=True)
             self.logger.info(f"Pruned {sum(opt_ts_mol.KeepIDs.values()) - len(unique_ids)} TS conformers")
-            opt_ts_mol = dict_to_mol(opt_ts_mol_dict, conf_copy_attrs=["KeepIDs", "energy"])
             opt_ts_mol.KeepIDs = {k: k in unique_ids and v for k, v in opt_ts_mol.KeepIDs.items()}
             with open(os.path.join(self.save_dir, "prune_check_ids.pkl"), "wb") as f:
                 pickle.dump(opt_ts_mol.KeepIDs, f)
