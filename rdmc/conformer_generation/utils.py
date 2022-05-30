640489803216f7141dd1d2f33a3342d7297bad15
Lagnajit Pattanaik <lagnajit@mit.edu>
2022-05-30 06:37:43 -0400

    add helper function to check failure modes from ts conformer generation

--- rdmc/conformer_generation/utils.py
+++ rdmc/conformer_generation/utils.py
@@ -8,6 +8,10 @@ Utilities for conformer generation modules
 from rdkit.Chem import AllChem
 from rdkit.ML.Cluster import Butina
 
+import os
+import pickle
+import numpy as np
+from collections import defaultdict
 
 def mol_to_dict(mol,
                 copy: bool = True,
@@ -76,3 +80,48 @@ def cluster_confs(mol, cutoff=1.0):
     [updated_mol.AddConformer(c.ToConformer(), assignId=True) for c in mol.GetConformers(confs_to_keep)]
 
     return updated_mol
+
+
+def get_conf_failure_mode(rxn_dir):
+    """
+    Parse a reaction directory for a TS generation run and extract failure modes (which conformer failed the
+    full workflow and for what reason)
+
+    Args:
+        rxn_dir (str) Path to the reaction directory.
+
+    Returns:
+        failure_dict ('dict'): Dictionary of conformer ids mapped to the corresponding failure mode.
+    """
+
+    failure_modes = {
+        0: "opt",
+        1: "prune",
+        2: "freq",
+        3: "irc",
+        4: "workflow",
+        5: "none",
+    }
+
+    opt_check_file = os.path.join(rxn_dir, "opt_check_ids.pkl")
+    freq_check_file = os.path.join(rxn_dir, "freq_check_ids.pkl")
+    prune_check_file = os.path.join(rxn_dir, "prune_check_ids.pkl")
+    irc_check_file = os.path.join(rxn_dir, "irc_check_ids.pkl")
+    workflow_check_file = os.path.join(rxn_dir, "workflow_check_ids.pkl")
+
+    opt_check_ids = pickle.load(open(opt_check_file, "rb"))
+    prune_check_ids = pickle.load(open(prune_check_file, "rb"))
+    freq_check_ids = pickle.load(open(freq_check_file, "rb"))
+    irc_check_ids = pickle.load(open(irc_check_file, "rb"))
+    workflow_check_ids = pickle.load(open(workflow_check_file, "rb"))
+
+    all_checks = defaultdict(list)
+    for d in [opt_check_ids, prune_check_ids, freq_check_ids, irc_check_ids, workflow_check_ids]:
+        for k, v in d.items():
+            all_checks[k].append(v)
+
+    all_checks = np.concatenate([np.array([*all_checks.values()]), np.array([[False]] * len(all_checks))], axis=1)
+    modes = np.argmax(~all_checks, axis=1)
+    failure_dict = {i: failure_modes[m] for i, m in enumerate(modes)}
+
+    return failure_dict
