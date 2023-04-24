335e03f440237656d942449ce0007fd99ff8cbce
Xiaorui Dong <xiaorui@mit.edu>
2023-04-24 16:48:03 -0400

    Create the GaussianOptimizer
    This is a re-write of the original GaussianOptimizer in optimizers.py.

--- rdmc/conformer_generation/optimizer/__init__.py
+++ rdmc/conformer_generation/optimizer/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from. gaussian import GaussianOptimizer
 from .forcefield import MMFFOptimizer
 from .orca import ORCAOptimizer
 from .xtb import XTBOptimizer
