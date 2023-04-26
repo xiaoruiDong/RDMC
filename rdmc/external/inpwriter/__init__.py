36f0b4f51b6550f860e976eb9162dff64453aa36
Xiaorui Dong <xiaorui@mit.edu>
2023-04-26 01:31:52 -0400

    Make qchem inpwriter easy to import

--- rdmc/external/inpwriter/__init__.py
+++ rdmc/external/inpwriter/__init__.py
@@ -5,6 +5,9 @@ from .gaussian import (write_gaussian_opt,
                        write_gaussian_freq,
                        write_gaussian_irc,
                        write_gaussian_gsm)
+from .qchem import (write_qchem_opt,
+                    write_qchem_freq,
+                    write_qchem_irc)
 from .orca import (write_orca_opt,
                    write_orca_freq,
                    write_orca_irc,
