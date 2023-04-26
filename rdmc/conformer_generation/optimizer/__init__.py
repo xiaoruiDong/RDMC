7e2de4d3869281bcb27e3d4c9c9627fd0c749bbc
Xiaorui Dong <xiaorui@mit.edu>
2023-04-26 03:05:37 -0400

    Add a new Sella optimizer using the Sella module by Zador group

--- rdmc/conformer_generation/optimizer/__init__.py
+++ rdmc/conformer_generation/optimizer/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from. gaussian import GaussianOptimizer
 from .forcefield import MMFFOptimizer
+from. gaussian import GaussianOptimizer
 from .orca import ORCAOptimizer
 from .qchem import QChemOptimizer
+from .sella import SellaOptimizer
 from .xtb import XTBOptimizer
