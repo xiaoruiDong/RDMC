279f40eb49cb04b2871d4efceb14214b4f73778c
Xiaorui Dong <xiaorui@mit.edu>
2023-04-27 17:34:08 -0400

    Add easier way to import QchemBaseTask, ORCABaseTask, XTBBaseTask

--- rdmc/conformer_generation/task/__init__.py
+++ rdmc/conformer_generation/task/__init__.py
@@ -4,3 +4,6 @@
 from .base import Task
 from .gaussian import GaussianBaseTask
 from .molio import MolIOTask
+from .orca import ORCABaseTask
+from .qchem import QChemBaseTask
+from .xtb import XTBBaseTask
