a5d2b34773c465a0cf09aa0c642cc3bc294c2b1b
Xiaorui Dong <xiaorui@mit.edu>
2023-04-28 00:27:49 -0400

    Simplify orca optimizer with ORCABaseTask and IOOptimizer classes

--- rdmc/conformer_generation/optimizer/orca.py
+++ rdmc/conformer_generation/optimizer/orca.py
@@ -1,143 +1,129 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import os.path as osp
-import shutil
-import subprocess
-import tempfile
-from typing import Optional
-
 import numpy as np
 
-from rdmc import RDKitMol
-from rdmc.conformer_generation.optimizer.base import BaseOptimizer
-from rdmc.conformer_generation.utils import get_binary
-from rdmc.external.logparser import ORCALog
-from rdmc.external.inpwriter import write_orca_opt
-
-orca_binary = get_binary('orca')
+from rdmc.conformer_generation.optimizer.base import IOOptimizer
+from rdmc.conformer_generation.task import ORCABaseTask
 
 
-class ORCAOptimizer(BaseOptimizer):
+class ORCAOptimizer(ORCABaseTask, IOOptimizer):
     """
     The class to optimize geometries using the algorithm built in ORCA.
     You have to have the Orca package installed to run this optimizer.
-    # todo: make a general optimizer for Gaussian, QChem, and ORCA
 
     Args:
-        method (str, optional): The method available in ORCA to be used for TS optimization.
-                                If you want to use XTB methods, you need to put the xtb binary into the Orca directory. Defaults to XTB2.
+        method (str, optional): The method available in ORCA to be used for optimization.
+                                If you want to use XTB methods, you need to put the xtb binary
+                                into the Orca directory. Defaults to XTB2.
         nprocs (int, optional): The number of processors to use. Defaults to 1.
         memory (int, optional): The memory to use in GB. Defaults to 1.
     """
 
-    request_external_software = ['orca']
+    label = 'ORCAOptimizer'
+    subtask_dir_name = 'orca_opt'
+    files = {'input_file': 'orca_opt.inp',
+             'log_file': 'orca_opt.log',
+             'opt_file': 'orca_opt.opt'}
+    keep_files = ['orca_opt.inp', 'orca_opt.log', 'orca_opt.opt']
+    create_mol_flag = True
+    init_attrs = {'energies': np.nan, 'frequencies': None}
+    calc_type = 'opt'
 
-    def task_prep(self,
-                  method: str = "GFN2-xTB",
-                  nprocs: int = 1,
-                  memory: int = 1,
-                  **kwargs,
-                  ):
-        self.method = method
-        self.nprocs = nprocs
-        self.memory = memory
-
-    @BaseOptimizer.timer
-    def run(self,
-            mol: 'RDKitMol',
-            ts: bool = False,
-            multiplicity: Optional[int] = None,
-            charge: Optional[int] = None,
-            **kwargs):
+    def save_data(self, **kwargs):
         """
-        Optimize the geometries or TS guesses.
-
-        Args:
-            mol (RDKitMol): An RDKitMol object with all guess geometries embedded as conformers.
-            ts (bool, optional): Whether the molecule is a TS. Defaults to False.
-            multiplicity (int): The multiplicity of the molecule. Defaults to None, to use the multiplicity of mol.
-            charge (int): The charge of the molecule. Defaults to None, to use the charge of mol.
-
-        Returns:
-            RDKitMol
+        Save the data.
         """
-        mult, charge = self._get_mult_and_chrg(mol, multiplicity, charge)
+        super(ORCABaseTask, self).save_data(**kwargs)  # from IOOptimizer
 
-        run_ids = getattr(mol, 'keep_ids', [True] * mol.GetNumConformers())
-        work_dir = osp.abspath(self.save_dir) if self.save_dir else tempfile.mkdtemp()
-        subtask_dirs = [osp.join(work_dir, f"orca_opt{cid}") for cid in range(len(run_ids))]
-        input_paths = [osp.join(subtask_dirs[cid], "orca_opt.inp") for cid in range(len(run_ids))]
-        log_paths = [osp.join(subtask_dirs[cid], "orca_opt.log") for cid in range(len(run_ids))]
-        opt_xyzs = [osp.join(subtask_dirs[cid], "orca_opt.xyz") for cid in range(len(run_ids))]
+    def post_run(self, **kwargs):
+        """
+        Setting the success information, also set the energy to the
+        conformers. Remove temporary directory if necessary.
+        """
+        super(ORCABaseTask, self).post_run(**kwargs)  # from IOOptimizer
 
-        new_mol = mol.Copy(copy_attrs=[])
-        new_mol.keep_ids = [False] * len(run_ids)
-        new_mol.energies = [np.nan] * len(run_ids)
-        new_mol.frequencies = [None] * len(run_ids)
+    def analyze_subtask_result(self,
+                               mol: 'RDKitMol',
+                               subtask_id: int,
+                               subtask_result: tuple,
+                               **kwargs):
+        """
+        Analyze the subtask result. This method will parse the number of optimization
+        cycles and the energy from the xTB output file and set them to the molecule.
+        """
+        try:
+            log = self.logparser(self.paths['log_file'][subtask_id])
+            if log.success:
+                try:
+                    mol.SetPositions(log.converged_geometries[-1],
+                                     id=subtask_id)
+                except IndexError:
+                    # ORCA treats the following as converged job as well while not fulfilling
+                    # all of the convergence criteria. This is currently treated as non-converged
+                    # by the ORCALog parser. Therefore, we need to catch this exception.
+                    #
+                    #                         .--------------------.
+                    #   ----------------------|Geometry convergence|-------------------------
+                    #   Item                value                   Tolerance       Converged
+                    #   ---------------------------------------------------------------------
+                    #   Energy change      -0.0000139792            0.0000050000      NO
+                    #   RMS gradient        0.0000407031            0.0001000000      YES
+                    #   MAX gradient        0.0000648215            0.0003000000      YES
+                    #   RMS step            0.0000999215            0.0020000000      YES
+                    #   MAX step            0.0001589253            0.0040000000      YES
+                    #   ........................................................
+                    #   Max(Bonds)      0.0001      Max(Angles)    0.00
+                    #   Max(Dihed)        0.00      Max(Improp)    0.00
+                    #   ---------------------------------------------------------------------
+                    #
+                    #    Everything but the energy has converged. However, the energy
+                    #    appears to be close enough to convergence to make sure that the
+                    #    final evaluation at the new geometry represents the equilibrium energy.
+                    #    Convergence will therefore be signaled now
+                    #
+                    # todo: 1. wait until updates in cclib. or
+                    # todo: 2. in ORCALog force the last geometry to be converged if the following is detected.
+                    # ***********************HURRAY********************
+                    # ***        THE OPTIMIZATION HAS CONVERGED     ***
+                    # *************************************************
+                    mol.SetPositions(log.all_geometries[-1],
+                                     id=subtask_id)
 
-        # Generate and save the ORCA input file
-        for cid, keep_id in enumerate(run_ids):
-            if not keep_id:
-                continue
+        except Exception as exc:
+            mol.keep_ids[subtask_id] = False
+            print(f'ORCA optimization failed:\n{exc}')
+        else:
             try:
-                os.makedirs(subtask_dirs[cid], exist_ok=True)
-                orca_input = write_orca_opt(mol=mol,
-                                            conf_id=cid,
-                                            ts=ts,
-                                            charge=charge,
-                                            mult=mult,
-                                            method=self.method,
-                                            memory=self.memory,
-                                            **kwargs,
-                                            )
-                with open(input_paths[cid], 'w') as f_inp:
-                    f_inp.write(orca_input)
-            except Exception as exc:
-                print(f'ORCA opt input file generation failed:\n{exc}')
+                mol.energies[subtask_id] = log.get_scf_energies(relative=False)[-1].item()
+            except AttributeError:
+                # Enable to parse the energies of the xTB methods.
+                try:
+                    mol.energies[subtask_id] = \
+                            parse_energy_from_opt_file(self.paths['opt_file'][subtask_id])
+                except FileNotFoundError:
+                    mol.energies[subtask_id] = np.nan
+                    # todo: log energy value not found
+            mol.frequencies[subtask_id] = log.freqs
 
-        # Separate for loop
-        # Todo: parallelize this
-        for cid, keep_id in enumerate(run_ids):
-            if not keep_id:
-                continue
 
-            # Run the optimization using subprocess
-            try:
-                with open(log_paths[cid], "w") as f_log:
-                    orca_run = subprocess.run(
-                        args=[orca_binary, input_paths[cid]],
-                        stdout=f_log,
-                        stderr=subprocess.STDOUT,
-                        cwd=subtask_dirs[cid],
-                        check=True,
-                    )
-            except Exception as exc:
-                print(f'ORCA optimization failed:\n{exc}')
-                continue
+def parse_energy_from_opt_file(path: str):
+    """
+    Parse the energy from the opt file.
 
-            # Check the Orca results
-            try:
-                orca_log = ORCALog(log_paths[cid])
-                if orca_log.success:
-                    opt_mol = RDKitMol.FromFile(opt_xyzs[cid], sanitize=ts)
-                    new_mol.SetPositions(opt_mol.GetPositions(), id=cid)
-                    new_mol.keep_ids[cid] = True
-                    # If the energy and frequency are not available
-                    # or the parsing failed, # the keep_ids and geometries
-                    # will be still correctly set, respectively.
-                    new_mol.energies[cid] = \
-                                orca_log.get_scf_energies(converged=True,
-                                                          relative=False)[-1].item()
-                    new_mol.frequencies[cid] = orca_log.freqs
-                else:
-                    print('ORCA optimization succeeded but log parsing failed.')
-            except Exception as exc:
-                print(f'ORCA optimization succeeded but log parsing failed:\n{exc}')
-
-        # Clean up
-        if not self.save_dir:
-            shutil.rmtree(work_dir)
-
-        return new_mol
+    Args:
+        path (str): The path to the opt file (e.g., orca_opt.opt).
+    """
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    for i, line in enumerate(lines):
+        if line == '$energies':
+            # E.g.,
+            # $energies
+            # 4
+            # -4.1738410246499997
+            # -4.1750652237499999
+            # -4.1751853899600002
+            # -4.1752182364700001
+            n_iter = int(lines[i + 1].strip())
+            return float(lines[i + n_iter + 1].strip()) * 627.509  # hartree to kcal/mol
