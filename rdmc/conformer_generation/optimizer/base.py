834070ead8f0937972dd80df53ee37c6ff2b7cd3
Xiaorui Dong <xiaorui@mit.edu>
2023-04-30 16:44:01 -0400

    Update BaseOptimizer and IOOptimizer with MolTask/MolIOTask

--- rdmc/conformer_generation/optimizer/base.py
+++ rdmc/conformer_generation/optimizer/base.py
@@ -2,74 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import os.path as osp
-import shutil
-from typing import Optional
 
-from rdmc.conformer_generation.task import Task, MolIOTask
+import numpy as np
+
+from rdmc.conformer_generation.task import MolTask, MolIOTask
 from rdmc.conformer_generation.utils import mol_to_sdf
 
 
-class BaseOptimizer(Task):
-
-    def pre_run(self, mol, **kwargs):
-        """
-        Set the number of conformers to be optimized to n_subtasks.
-        """
-        try:
-            mol.keep_ids = [bool(conf.GetProp('KeepID'))
-                            for conf in mol.GetAllConformers()]
-            self.n_subtasks = sum(mol.keep_ids)
-        except KeyError:
-            # No keepID property, assume all conformers are to be optimized
-            self.n_subtasks = mol.GetNumConformers()
-
-    def post_run(self, **kwargs):
-        """
-        Set the energy and keepid to conformers.
-        """
-        mol = self.last_result
-        for conf, keep_id, e in zip(mol.GetAllConformers(),
-                                    mol.keep_ids,
-                                    mol.energies):
-            conf.SetDoubleProp("Energy", e)
-            conf.SetBoolProp("KeepID", keep_id)
-            conf.SetBoolProp(f"{self.label}Success", keep_id)
-        self.n_success = sum(mol.keep_ids)
-
-    def save_data(self, **kwargs):
-        """
-        Set the SMILES as the name of the RDKitMol object.
-        """
-        mol = self.last_result
-        path = osp.join(self.save_dir, "optimized_confs.sdf")
-
-        mol_to_sdf(mol=mol,
-                   path=path,)
-
-    def run(self, **kwargs):
-        """
-        Run the optimization task.
-        You need to implement this method in the child class.
-        a molecule need to be returned as the result, and
-        mol.keep_ids (if succeed) and mol.energies should be set in this method.
-        """
-        raise NotImplementedError
-
-    @staticmethod
-    def _get_mult_and_chrg(mol: 'RDKitMol',
-                           multiplicity: Optional[int],
-                           charge: Optional[int]):
-        """
-        Use the multiplicity and charge from the molecule if not specified
-        """
-        if multiplicity is None:
-            multiplicity = mol.GetSpinMultiplicity()
-        if charge is None:
-            charge = mol.GetFormalCharge()
-        return multiplicity, charge
-
-
-class IOOptimizer(MolIOTask):
+class BaseOptimizer(MolTask):
+    """
+    An abstract class for tasks that optimize conformers. Compared to MolTask,
+    it saves the optimized conformers (optimized_confs.sdf) to the save_dir.
+    """
 
     def post_run(self, **kwargs):
         """
@@ -77,17 +21,61 @@ class IOOptimizer(MolIOTask):
         conformers. Remove temporary directory if necessary.
         """
         super().post_run(**kwargs)
-        mol = self.last_result
-        for conf, energy in zip(mol.GetAllConformers(),
-                                mol.energies):
-            conf.SetDoubleProp("Energy", energy)
+        assign_energy_to_conformer(self.last_result)
 
     def save_data(self, **kwargs):
         """
         Set the SMILES as the name of the RDKitMol object.
         """
-        mol = self.last_result
         path = osp.join(self.save_dir, "optimized_confs.sdf")
-
-        mol_to_sdf(mol=mol,
+        mol_to_sdf(mol=self.save_dir,
                    path=path,)
+
+
+class IOOptimizer(MolIOTask):
+    """
+    An abstract class for tasks that optimize conformers and involve I/O operation.
+    Compared to MolTask, it saves the optimized conformers (optimized_confs.sdf) to
+    the save_dir. Besides, since some programs also generated Hessian results during
+    optimization, this class will also saves the frequencies if available.
+    """
+
+    # By default, create a copy of the input molecule in optimization. So, the
+    # original molecule will not be modified.
+    copy_mol_flag = True
+    # The type of calculation. This attribute may be used by the class to write
+    # input files or parse output files.
+    calc_type = 'opt'
+    # The attributes and their initial values to be attached to the mol object before
+    # the task. In optimization, the energies and frequencies are expected to be
+    # calculated. So, they are included in the init_attrs.
+    init_attrs = {'energies': np.nan, 'frequencies': None}
+
+    def post_run(self, **kwargs):
+        """
+        Besides setting the success information, also set the energy to the
+        conformers. Remove temporary directory if necessary.
+        """
+        super().post_run(**kwargs)
+        assign_energy_to_conformer(self.last_result)
+
+    def save_data(self, **kwargs):
+        """
+        Set the SMILES as the name of the RDKitMol object.
+        """
+        path = osp.join(self.save_dir, "optimized_confs.sdf")
+        mol_to_sdf(mol=self.last_result,
+                   path=path,)
+
+
+def assign_energy_to_conformer(mol: 'RDKitMol'):
+    """
+    Assign energy to conformer. This is a helper function assigning energies in
+    mol.energies to conformer's Energy property.
+
+    Args:
+        mol (RDKitMol): The molecule to be operated on.
+    """
+    for conf, energy in zip(mol.GetAllConformers(),
+                            mol.energies):
+        conf.SetDoubleProp("Energy", energy)
