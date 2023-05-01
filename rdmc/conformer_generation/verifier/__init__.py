b9ad0bb560a7dc21abfb100db0de997118b8d078
Xiaorui Dong <xiaorui@mit.edu>
2023-05-01 16:57:26 -0400

    Add an easier way to import verifiers from the module

--- rdmc/conformer_generation/verifier/__init__.py
+++ rdmc/conformer_generation/verifier/__init__.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+from .connect import ConnectivityVerifier
+from .freq import FreqVerifier
+from .gaussian import GaussianFreqVerifier
+from .orca import ORCAFreqVerifier
+from .qchem import QChemFreqVerifier
+from .xtb import XTBFreqVerifier
