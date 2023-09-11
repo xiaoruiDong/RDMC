2abdc02675b9abebd02b3a3765c7f6c0ff37e462
Xiaorui Dong <xiaorui@mit.edu>
2023-09-11 11:49:43 -0400

    Update index page and add cite/credits pages
    1. Update the index with new introduction paragraph, installation guide, developer information
    2. add a how to cite page
    3. add a credits page
    4. add developer's social media info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import datetime
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -72,6 +71,9 @@ napoleon_custom_sections = [('Returns', 'params_style')]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
 
 # -- Packages that are not importable from the default environment ---------
 
