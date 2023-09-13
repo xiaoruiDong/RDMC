dacf738662762d03d35b6e267d8df5d37fe36471
Xiaorui Dong <xiaorui@mit.edu>
2023-09-13 16:33:35 -0400

    Fix generate resonance structure and update unit test
    1. Update how flags are used in generating the resonance structure and sanitize kekulized molecules
    2. Update a method to remove atom highlighting after filtration
    3. Avoid `Kekulize` to reassigning bond orders
    4. Correct default arg and docstring
    5. In unit tests, use benzene, 1-ethyl benzene radical, and C=C[CH]C=C as examples. Remove phenyl as the default RDKit behavior is not as expected.

--- test/test_mol.py
+++ test/test_mol.py
@@ -609,43 +609,101 @@ class TestRDKitMol(unittest.TestCase):
                     RDKitMol.FromSmiles(smi)
                 )
 
-        # Test case 1: 1-Phenylethyl radical
+        # Test case 1: benzene
+        smi = 'c1ccccc1'
+        mol = RDKitMol.FromSmiles(smi)
+        # Without kekulization, RDKit returns 1 resonance structures
+        assert len(generate_radical_resonance_structures(
+            mol,
+            unique=False,
+            kekulize=False,
+        )) == 1
+        # With kekulization, RDKit returns 2 resonance structures
+        assert len(generate_radical_resonance_structures(
+            mol,
+            unique=False,
+            kekulize=True,
+        )) == 2
+        # With kekulization, RDKit returns 1 resonance structures
+        # during uniquifyication without considering atom map
+        assert len(generate_radical_resonance_structures(
+            mol,
+            unique=True,
+            consider_atommap=False,
+            kekulize=True,
+        )) == 1
+        # With kekulization, RDKit returns 2 resonance structures
+        # during uniquifyication with considering atom map
+        assert len(generate_radical_resonance_structures(
+            mol,
+            unique=True,
+            consider_atommap=True,
+            kekulize=True,
+        )) == 2
+
+        # Test case 2: 1-Phenylethyl radical
         smi = 'c1ccccc1[CH]C'
-        # Without filtration, RDKit returns 5 resonance structures
+        mol = RDKitMol.FromSmiles(smi)
+        # Without kekulization, RDKit returns 4 resonance structures
+        # 3 with radical site on the ring and 1 with differently kekulized benzene
+        assert len(generate_radical_resonance_structures(
+            mol,
+            unique=False,
+            kekulize=False,
+        )) == 4
+        # With kekulization, RDKit returns 5 resonance structures
         # 3 with radical site on the ring and 2 with differently kekulized benzene
         assert len(generate_radical_resonance_structures(
-            RDKitMol.FromSmiles(smi),
-            unique=False
+            mol,
+            unique=False,
+            kekulize=True,
         )) == 5
-        # With filtration and not considering atom map, RDKit returns 3 structures
+        # With filtration, kekulization, and not considering atom map,
+        # There will be 3 resonance structures, 2 with radical site on the ring
+        # and 1 with radial site on the alkyl chain
         assert len(generate_radical_resonance_structures(
             RDKitMol.FromSmiles(smi),
             unique=True,
-            consider_atommap=False
+            consider_atommap=False,
+            kekulize=True,
         )) == 3
-        # With filtration and considering atom map, RDKit returns 4 structures
+        # With filtration and considering atom map, and without kekulization,
+        # RDKit returns 4 structures, 3 with radical site on the ring
+        # and 1 with radial site on the alkyl chain
         assert len(generate_radical_resonance_structures(
             RDKitMol.FromSmiles(smi),
             unique=True,
             consider_atommap=True
         )) == 4
-        # Test case 2: Phenyl radical
-        smi = '[c:1]1[c:2]([H:7])[c:3]([H:8])[c:4]([H:9])[c:5]([H:10])[c:6]1[H:11]'
-        # Without filtration, RDKit returns 3 resonance structures
+
+        # Test case 3: Phenyl radical
+        smi = 'C=C[CH]C=C'
+        # No dependence on kekulization
         assert len(generate_radical_resonance_structures(
             RDKitMol.FromSmiles(smi),
-            unique=False
+            unique=False,
+            kekulize=True,
+        )) == len(generate_radical_resonance_structures(
+            RDKitMol.FromSmiles(smi),
+            unique=False,
+            kekulize=False,
+        )) == 3
+        # With filtering and considering atom map, RDKit returns 3 resonance structures
+        # radical site at two ends and the middle
+        assert len(generate_radical_resonance_structures(
+            RDKitMol.FromSmiles(smi),
+            unique=True,
+            consider_atommap=True,
+            kekulize=True,
         )) == 3
         # With filtration and not considering atom map, RDKit returns 2 structures
-        res_mols = generate_radical_resonance_structures(
+        # radical site at the end and the middle
+        assert len(generate_radical_resonance_structures(
             RDKitMol.FromSmiles(smi),
             unique=True,
             consider_atommap=False,
-        )
-        assert len(res_mols) == 2
-        # The first one (itself) should be aromatic and the second should not
-        for i, value in zip([0, 1], [True, False]):
-            assert res_mols[i].GetAtomWithIdx(0).GetIsAromatic() == value
+            kekulize=True,
+        )) == 2
 
 
 if __name__ == '__main__':
