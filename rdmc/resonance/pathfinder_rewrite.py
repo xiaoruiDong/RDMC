eaf5c44096f4e0a8b30d8816ceff50e69e025483
Xiaorui Dong <xiaorui@mit.edu>
2023-11-09 14:23:59 -0500

    Add a header for pathfinder_rewrite

--- rdmc/resonance/pathfinder_rewrite.py
+++ rdmc/resonance/pathfinder_rewrite.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This module includes an RDKit-based rewrite of the RMG resonance pathfinder.
+"""
+
 from typing import List
 
 from rdkit import Chem
