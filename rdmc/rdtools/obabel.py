3787831e3daaa0f58798fa23d3bedf5b2eb6b967
Xiaorui Dong <xiaorui@mit.edu>
2023-12-04 23:55:13 -0500

    Add openbabel module to rdtools
    Move openbabel functions from utils to rdtools/obabel

---
+++ rdmc/rdtools/obabel.py
@@ -0,0 +1,309 @@
+import numpy as np
+
+from rdkit import Chem
+
+try:
+    # Openbabel 3
+    from openbabel import openbabel as ob
+except ImportError:
+    # Openbabel 2
+    import openbabel as ob
+
+from rdmc.rdtools.conf import set_conformer_coordinates, add_conformer
+from rdmc.rdtools.general import BOND_ORDERS
+
+RDKIT_TO_OB_BOND_ORDER = {
+    Chem.BondType.SINGLE: 1,
+    Chem.BondType.DOUBLE: 2,
+    Chem.BondType.TRIPLE: 3,
+    Chem.BondType.QUADRUPLE: 4,
+    Chem.BondType.AROMATIC: 5,
+}
+
+
+def get_obmol_coords(obmol: ob.OBMol):
+    """
+    Get the atom coordinates from an openbabel molecule. If all coordinates are zero,
+    None will be returned.
+
+    Args:
+        obmol (OBMol): The openbabel OBMol to get coordinates from.
+
+    Returns:
+        np.array: The coordinates.
+    """
+    coords = []
+    for obatom in ob.OBMolAtomIter(obmol):
+        coords.append([obatom.GetX(), obatom.GetY(), obatom.GetZ()])
+    return np.array(coords)
+
+
+def openbabel_mol_to_rdkit_mol(
+    obmol: "openbabel.OBMol",
+    remove_hs: bool = False,
+    sanitize: bool = True,
+    embed: bool = True,
+) -> Chem.RWMol:
+    """
+    Convert a OpenBabel molecular structure to a Chem.rdchem.RWMol object.
+    Args:
+        obmol (Molecule): An OpenBabel Molecule object for the conversion.
+        remove_hs (bool, optional): Whether to remove hydrogen atoms from the molecule, Defaults to False.
+        sanitize (bool, optional): Whether to sanitize the RDKit molecule. Defaults to True.
+        embed (bool, optional): Whether to embeb 3D conformer from OBMol. Defaults to True.
+
+    Returns:
+        RWMol: A writable RDKit RWMol instance.
+    """
+    # For efficiency consideration, split keep_hs and remove_hs
+    if remove_hs:
+        return _obmol_to_rdkitmol_removehs(obmol, sanitize, embed)
+    else:
+        return _obmol_to_rdkitmol_keephs(obmol, sanitize, embed)
+
+
+def _obmol_to_rdkitmol_keephs(
+    obmol: ob.OBMol,
+    sanitize: bool,
+    embed: bool,
+) -> Chem.RWMol:
+    rw_mol = Chem.RWMol()
+    for obatom in ob.OBMolAtomIter(obmol):
+        atomic_num = obatom.GetAtomicNum()
+        atom = Chem.Atom(atomic_num)
+        isotope = obatom.GetIsotope()
+        if isotope != 0:
+            atom.SetIsotope(isotope)
+        spin = obatom.GetSpinMultiplicity()
+        atom.SetNoImplicit(True)
+        if spin == 2:  # radical
+            atom.SetNumRadicalElectrons(1)
+        elif spin in [1, 3]:  # carbene
+            # TODO: Not sure if singlet and triplet are distinguished
+            atom.SetNumRadicalElectrons(2)
+        atom.SetFormalCharge(obatom.GetFormalCharge())
+        rw_mol.AddAtom(atom)
+
+    for bond in ob.OBMolBondIter(obmol):
+        atom1_idx = bond.GetBeginAtomIdx()
+        atom2_idx = bond.GetEndAtomIdx()
+        # Get the bond order. For aromatic molecules, the bond order is not
+        # 1.5 but 1 or 2. Manually set them to 1.5
+        bond_order = bond.GetBondOrder()
+        if bond_order not in [1, 2, 3, 4] or bond.IsAromatic():
+            bond_order = 12
+        # Atom indexes in Openbabel is 1-indexed, so we need to convert them to 0-indexed
+        rw_mol.AddBond(atom1_idx - 1, atom2_idx - 1, BOND_ORDERS[bond_order])
+
+    if sanitize:
+        Chem.SanitizeMol(rw_mol)
+
+    # If OBMol has 3D information, it can be embed to the RDKit Mol
+    if embed and (obmol.HasNonZeroCoords() or obmol.NumAtoms() == 1):
+        coords = get_obmol_coords(obmol)
+        add_conformer(rw_mol, coords=coords)
+
+    return rw_mol
+
+
+def _obmol_to_rdkitmol_removehs(
+    obmol: ob.OBMol,
+    sanitize: bool,
+    embed: bool,
+) -> Chem.RWMol:
+    rw_mol = Chem.RWMol()
+    ob_rdkit_atoms = {}
+    total_num_atoms = 0
+    for i, obatom in enumerate(ob.OBMolAtomIter(obmol)):
+        atomic_num = obatom.GetAtomicNum()
+        if atomic_num == 1:
+            continue
+        atom = Chem.Atom(atomic_num)
+        isotope = obatom.GetIsotope()
+        if isotope != 0:
+            atom.SetIsotope(isotope)
+        spin = obatom.GetSpinMultiplicity()
+        if spin == 2:  # radical
+            atom.SetNumRadicalElectrons(1)
+        elif spin in [1, 3]:  # carbene
+            # TODO: Not sure if singlet and triplet are distinguished
+            atom.SetNumRadicalElectrons(2)
+        atom.SetFormalCharge(obatom.GetFormalCharge())
+        rw_mol.AddAtom(atom)
+        ob_rdkit_atoms[i + 1] = total_num_atoms
+        total_num_atoms += 1
+
+    for bond in ob.OBMolBondIter(obmol):
+        atom1_idx = bond.GetBeginAtomIdx()
+        atom2_idx = bond.GetEndAtomIdx()
+        if (
+            ob_rdkit_atoms.get(atom1_idx, -1) < 0
+            or ob_rdkit_atoms.get(atom2_idx, -1) < 0
+        ):
+            continue
+        # Get the bond order. For aromatic molecules, the bond order is not
+        # 1.5 but 1 or 2. Manually set them to 1.5
+        bond_order = bond.GetBondOrder()
+        if bond_order not in [1, 2, 3, 4] or bond.IsAromatic():
+            bond_order = 12
+        # Atom indexes in Openbabel is 1-indexed, so we need to convert them to 0-indexed
+        rw_mol.AddBond(atom1_idx - 1, atom2_idx - 1, BOND_ORDERS[bond_order])
+
+    # Rectify the molecule
+    Chem.RemoveHs(rw_mol, sanitize=sanitize)
+
+    # If OBMol has 3D information, it can be embed to the RDKit Mol
+    if embed and (obmol.HasNonZeroCoords() or obmol.NumAtoms() == 1):
+        coords = get_obmol_coords(obmol)
+        unmask_indices = np.array(sorted(ob_rdkit_atoms.keys())) - 1
+        add_conformer(rw_mol, coords=coords[unmask_indices])
+
+    return rw_mol
+
+
+def rdkit_mol_to_openbabel_mol(
+    rdmol: Chem.Mol,
+    embed: bool = True,
+) -> ob.OBMol:
+    """
+    Convert a Mol to an Openbabel mol. This a temporary replace of
+    ``rdkit_mol_to_openbabel_mol_manual``. When the molecule has multiple conformers,
+    this function will only use the first conformer.
+
+    Args:
+        rdmol (Mol): The RDKit Mol object to be converted.
+        embed (bool, optional): Whether to embed conformer into the OBMol. Defaults to ``True``.
+
+    Returns:
+        OBMol: An openbabel OBMol instance.
+    """
+    sdf_str = Chem.MolToMolBlock(rdmol)
+    obconv, obmol = ob.OBConversion(), ob.OBMol()
+    obconv.SetInFormat("sdf")
+    obconv.ReadString(obmol, sdf_str)
+
+    # Temporary Fix for spin multiplicity
+    _correct_atom_spin_mult(obmol)
+
+    if not embed:
+        for atom in ob.OBMolAtomIter(obmol):
+            atom.SetVector(ob.vector3(0, 0, 0))
+
+    return obmol
+
+
+def rdkit_mol_to_openbabel_mol_manual(
+    rdmol: Chem.Mol,
+    embed: bool = True,
+) -> ob.OBMol:
+    """
+    Convert a Mol/RWMol to a Openbabel mol. This function has a problem converting
+    aromatic molecules. Example: 'c1nc[nH]n1'. Currently use a workaround, converting an
+    RDKit Mol to sdf string and read by openbabel.
+
+    Args:
+        rdmol (Mol): The RDKit Mol/RWMol object to be converted.
+        embed (bool, optional): Whether to embed conformer into the OBMol. Defaults to True.
+
+    Returns:
+        OBMol: An openbabel OBMol instance.
+    """
+    obmol = ob.OBMol()
+    for rdatom in rdmol.GetAtoms():
+        obatom = obmol.NewAtom()
+        obatom.SetAtomicNum(rdatom.GetAtomicNum())
+        isotope = rdatom.GetIsotope()
+        if isotope != 0:
+            obatom.SetIsotope(isotope)
+        obatom.SetFormalCharge(rdatom.GetFormalCharge())
+        obatom.SetImplicitHCount(rdatom.GetTotalNumHs())
+
+    for bond in rdmol.GetBonds():
+        atom1_idx = bond.GetBeginAtomIdx() + 1
+        atom2_idx = bond.GetEndAtomIdx() + 1
+        order = RDKIT_TO_OB_BOND_ORDER[bond.GetBondType()]
+        obmol.AddBond(atom1_idx, atom2_idx, order)
+
+    # Note: aromatic is not correctly handled for
+    # heteroatom involved rings in the current molecule buildup.
+    # May need to update in the future
+
+    _correct_atom_spin_mult(obmol)
+
+    if embed:
+        try:
+            conf = rdmol.GetConformer()
+        except ValueError:
+            # No conformer
+            pass
+        else:
+            coords = conf.GetPositions()
+            set_obmol_coords(obmol, coords)
+
+    return obmol
+
+
+def set_obmol_coords(obmol: ob.OBMol, coords: np.array):
+    """
+    Get the atom coordinates from an openbabel molecule. If all coordinates are zero,
+    It will return None
+
+    Args:
+        obmol (OBMol): The openbabel OBMol to get coordinates from.
+        coords (np.array): The coordinates to set.
+    """
+    for atom_idx, atom in enumerate(ob.OBMolAtomIter(obmol)):
+        atom.SetVector(ob.vector3(*coords[atom_idx].tolist()))
+
+
+def parse_xyz_by_openbabel(xyz: str) -> ob.OBMol:
+    """
+    Perceive a xyz str using openbabel and generate the corresponding OBMol.
+
+    Args:
+        xyz (str): A str in xyz format containing atom positions.
+
+    Returns:
+        ob.OBMol: An openbabel molecule from the xyz
+    """
+    obconversion = ob.OBConversion()
+    obconversion.SetInFormat("xyz")
+    obmol = ob.OBMol()
+    success = obconversion.ReadString(obmol, xyz)
+    if not success:
+        raise ValueError("Unable to parse the provided xyz.")
+
+    # Temporary Fix for spin multiplicity
+    _correct_atom_spin_mult(obmol)
+
+    return obmol
+
+
+def _correct_atom_spin_mult(obmol: ob.OBMol):
+    """
+    This is a temporary Fix for Issue # 1. Openbabel has a native function called AssignSpinMultiplicity
+    however,  it becomes a dummy function in openbabel 3,
+    where it only returns True. See https://github.com/openbabel/
+    openbabel/blob/bcb790049bf43415a0b99adf725249c3f4da02bf/src/mol.cpp#L2389-L2395
+
+    The function implements a naive fix for the atom spin multiplicity in openbabel.
+    Currently, it cannot deal with any charged species properly.
+
+    Args:
+        obmol (OBMol): The openbabel OBMol to be fixed.
+    """
+    for obatom in ob.OBMolAtomIter(obmol):
+        atomic_num = obatom.GetAtomicNum()
+        total_valence = obatom.GetTotalValence()
+        # Find the unsaturated carbons
+        if atomic_num == 6 and total_valence < 4:
+            obatom.SetSpinMultiplicity(5 - total_valence)
+        # Find the unsaturated nitrogen
+        elif atomic_num == 7 and total_valence < 3:
+            obatom.SetSpinMultiplicity(4 - total_valence)
+        # Find the unsaturated oxygen
+        elif atomic_num == 8 and total_valence < 2:
+            obatom.SetSpinMultiplicity(3 - total_valence)
+        # Find the unsaturated nitrogen and halogen
+        elif atomic_num in [1, 9, 17, 35, 53] and total_valence == 0:
+            obatom.SetSpinMultiplicity(2)
---
+++ test/rdtools/test_obabel.py
@@ -0,0 +1,357 @@
+import pytest
+import numpy as np
+
+from rdkit import Chem
+
+try:
+    # Openbabel 3
+    from openbabel import openbabel as ob
+except ImportError:
+    # Openbabel 2
+    import openbabel as ob
+
+
+from rdmc.rdtools.obabel import (
+    get_obmol_coords,
+    openbabel_mol_to_rdkit_mol,
+    parse_xyz_by_openbabel,
+    rdkit_mol_to_openbabel_mol,
+    rdkit_mol_to_openbabel_mol_manual,
+    set_obmol_coords,
+)
+
+from rdmc.rdtools.atom import PERIODIC_TABLE
+
+
+smi_params = Chem.SmilesParserParams()
+smi_params.removeHs = False
+smi_params.sanitize = True
+
+
+def get_element_symbol(atomic_symbol):
+    return PERIODIC_TABLE.GetAtomicNumber(atomic_symbol)
+
+
+@pytest.mark.parametrize(
+    "xyz_str, xyz_coords",
+    [
+        (
+            """9
+
+C      0.877326   -0.098250   -0.084854
+C     -0.485434    0.500788    0.077366
+O     -1.467272   -0.400748   -0.379978
+H      0.761119   -1.199281   -0.258052
+H      1.505966    0.018086    0.835736
+H      1.450469    0.313605   -0.935789
+H     -0.650946    0.703458    1.166255
+H     -0.645038    1.419756   -0.523925
+H     -1.346190   -1.257412    0.103241""",
+            np.array(
+                [
+                    [0.87732605, -0.09825022, -0.08485408],
+                    [-0.48543355, 0.50078797, 0.0773663],
+                    [-1.46727204, -0.40074832, -0.3799782],
+                    [0.76111859, -1.19928127, -0.25805208],
+                    [1.50596584, 0.01808555, 0.83573634],
+                    [1.45046879, 0.3136052, -0.93578915],
+                    [-0.65094569, 0.70345754, 1.16625452],
+                    [-0.64503773, 1.41975555, -0.52392462],
+                    [-1.34619025, -1.25741199, 0.10324098],
+                ]
+            ),
+        )
+    ],
+)
+def test_get_obmol_coords(xyz_str, xyz_coords):
+    obconv, obmol = ob.OBConversion(), ob.OBMol()
+    obconv.SetInFormat("xyz")
+    obconv.ReadString(obmol, xyz_str)
+
+    np.testing.assert_array_almost_equal(
+        get_obmol_coords(obmol),
+        xyz_coords,
+        decimal=6,
+    )
+
+
+@pytest.mark.parametrize(
+    "xyz, smi, mult",
+    [
+        (
+            """8
+
+C     -0.763437    0.050567   -0.027960
+C      0.735495   -0.000842   -0.048377
+O      1.230196   -0.419073    1.194244
+H     -1.176457    1.066091   -0.162871
+H     -1.204191   -0.579978   -0.825262
+H     -1.138725   -0.301251    0.965053
+H      1.219517    0.947190   -0.311463
+H      1.097602   -0.762704   -0.783363""",
+            "CC[O]",
+            2,
+        ),
+        (
+            """6
+
+C     -0.387408    0.000432    0.000429
+N      0.944508   -0.436568    0.409251
+H     -0.855579   -0.809884   -0.612157
+H     -0.318201    0.948418   -0.574669
+H     -1.021874    0.176059    0.891897
+H      1.638555    0.121543   -0.114750""",
+            "C[NH]",
+            2,
+        ),
+        (
+            """4
+
+C     -0.003054    0.012107    0.249028
+H      1.038218   -0.178411   -0.082059
+H     -0.642509   -0.823783   -0.081409
+H     -0.392655    0.990087   -0.085560""",
+            "[CH3]",
+            2,
+        ),
+        (
+            """5
+
+C      0.001156   -0.019902   -0.015705
+H     -0.813529   -0.726520   -0.214171
+H     -0.307828    0.982159   -0.365147
+H      0.163077    0.057280    1.071021
+H      0.957126   -0.293016   -0.475997""",
+            "C",
+            1,
+        ),
+        (
+            """1
+
+Cl    0.000000    0.000000    0.000000""",
+            "[Cl]",
+            2,
+        ),
+    ],
+)
+def test_parse_xyz_by_openbabel(xyz, smi, mult):
+    obmol = parse_xyz_by_openbabel(xyz)
+
+    # Check molecule properties are correct
+    num_atoms = int(xyz.splitlines()[0].strip())
+    assert obmol.NumAtoms() == num_atoms
+    assert [obmol.GetAtomById(i).GetAtomicNum() for i in range(num_atoms)] == [
+        get_element_symbol(atom.strip().split()[0])
+        for atom in xyz.splitlines()[2 : 2 + num_atoms]
+    ]
+    assert obmol.GetTotalSpinMultiplicity() == mult
+
+    # Check if the coordinates are correctly converted
+    xyz_coords = get_obmol_coords(obmol)
+    assert len(xyz_coords) == num_atoms
+    np.testing.assert_array_almost_equal(
+        xyz_coords,
+        np.array(
+            [
+                [
+                    float(coord_values)
+                    for coord_values in coord_values.strip().split()[1:]
+                ]
+                for coord_values in xyz.splitlines()[2 : 2 + num_atoms]
+            ]
+        ),
+        decimal=6,
+    )
+
+    # Check if the molecule is correct
+    obconv = ob.OBConversion()
+    obconv.SetOutFormat("smi")
+    assert obconv.WriteString(obmol).strip() == smi
+
+
+@pytest.mark.parametrize(
+    "smi, xyz",
+    [
+        (
+            "CC[O]",
+            np.array(
+                [
+                    [-0.763437, 0.050567, -0.027960],
+                    [0.735495, -0.000842, -0.048377],
+                    [1.230196, -0.419073, 1.194244],
+                ]
+            ),
+        ),
+        ("C", np.array([[0.001156, -0.019902, -0.015705]])),
+        ("[Cl]", np.array([[0.0, 0.0, 0.0]])),
+    ],
+)
+def test_set_obmol_coords(smi, xyz):
+    obconv, obmol = ob.OBConversion(), ob.OBMol()
+    obconv.SetInFormat("smi")
+    obconv.ReadString(obmol, smi)
+
+    set_obmol_coords(obmol, xyz)
+
+    assert get_obmol_coords(obmol).shape == xyz.shape
+    np.testing.assert_array_almost_equal(
+        get_obmol_coords(obmol),
+        xyz,
+        decimal=6,
+    )
+
+
+@pytest.mark.parametrize(
+    "smi",
+    [
+        "CC[O]",
+        "C[NH]",
+        "[CH3]",
+        "C",
+    ],
+)
+@pytest.mark.parametrize("embed", [True, False])
+def test_rdkit_mol_to_openbabel_mol(smi, embed):
+    mol = Chem.MolFromSmiles(smi, smi_params)
+    if embed:
+        Chem.AllChem.EmbedMolecule(mol)
+
+    obmol = rdkit_mol_to_openbabel_mol(mol, embed=embed)
+    assert obmol.NumAtoms() == mol.GetNumAtoms()
+    obconv = ob.OBConversion()
+    obconv.SetOutFormat("smi")
+    assert obconv.WriteString(obmol).strip() == smi
+
+    if embed:
+        np.testing.assert_array_almost_equal(
+            get_obmol_coords(obmol),
+            mol.GetConformer().GetPositions(),
+            decimal=4,  # limited by RDKit MolToMolBlock
+        )
+
+
+@pytest.mark.parametrize(
+    "smi",
+    [
+        "CC[O]",
+        "C[NH]",
+        "[CH3]",
+        "C",
+    ],
+)
+@pytest.mark.parametrize("embed", [True, False])
+def test_rdkit_mol_to_openbabel_mol_manual(smi, embed):
+    mol = Chem.MolFromSmiles(smi, smi_params)
+    if embed:
+        Chem.AllChem.EmbedMolecule(mol)
+
+    obmol = rdkit_mol_to_openbabel_mol_manual(mol, embed=embed)
+    assert obmol.NumAtoms() == mol.GetNumAtoms()
+    obconv = ob.OBConversion()
+    obconv.SetOutFormat("smi")
+    assert obconv.WriteString(obmol).strip() == smi
+
+    if embed:
+        np.testing.assert_array_almost_equal(
+            get_obmol_coords(obmol),
+            mol.GetConformer().GetPositions(),
+            decimal=6,
+        )
+
+
+@pytest.mark.parametrize(
+    "xyz, smi, mult",
+    [
+        (
+            """8
+
+C     -0.763437    0.050567   -0.027960
+C      0.735495   -0.000842   -0.048377
+O      1.230196   -0.419073    1.194244
+H     -1.176457    1.066091   -0.162871
+H     -1.204191   -0.579978   -0.825262
+H     -1.138725   -0.301251    0.965053
+H      1.219517    0.947190   -0.311463
+H      1.097602   -0.762704   -0.783363""",
+            "CC[O]",
+            2,
+        ),
+        (
+            """6
+
+C     -0.387408    0.000432    0.000429
+N      0.944508   -0.436568    0.409251
+H     -0.855579   -0.809884   -0.612157
+H     -0.318201    0.948418   -0.574669
+H     -1.021874    0.176059    0.891897
+H      1.638555    0.121543   -0.114750""",
+            "C[NH]",
+            2,
+        ),
+        (
+            """4
+
+C     -0.003054    0.012107    0.249028
+H      1.038218   -0.178411   -0.082059
+H     -0.642509   -0.823783   -0.081409
+H     -0.392655    0.990087   -0.085560""",
+            "[CH3]",
+            2,
+        ),
+        (
+            """5
+
+C      0.001156   -0.019902   -0.015705
+H     -0.813529   -0.726520   -0.214171
+H     -0.307828    0.982159   -0.365147
+H      0.163077    0.057280    1.071021
+H      0.957126   -0.293016   -0.475997""",
+            "C",
+            1,
+        ),
+        (
+            """1
+
+Cl    0.000000    0.000000    0.000000""",
+            "[Cl]",
+            2,
+        ),
+    ],
+)
+@pytest.mark.parametrize("remove_hs", [True, False])
+@pytest.mark.parametrize("sanitize", [True, False])
+@pytest.mark.parametrize("embed", [True, False])
+def test_openbabel_mol_to_rdkit_mol(xyz, smi, mult, remove_hs, sanitize, embed):
+    obmol = parse_xyz_by_openbabel(xyz)
+    mol = openbabel_mol_to_rdkit_mol(
+        obmol,
+        remove_hs=remove_hs,
+        sanitize=sanitize,
+        embed=embed,
+    )
+    if remove_hs:
+        assert mol.GetNumAtoms() == obmol.NumHvyAtoms()
+    else:
+        assert mol.GetNumAtoms() == obmol.NumAtoms()
+    assert sum([atom.GetNumRadicalElectrons() for atom in mol.GetAtoms()]) + 1 == mult
+    assert mol.GetNumConformers() == 0 if not embed else 1
+
+    if embed and not remove_hs:
+        np.testing.assert_array_almost_equal(
+            mol.GetConformer().GetPositions(),
+            get_obmol_coords(obmol),
+            decimal=6,
+        )
+    elif embed:
+        heavy_atom_idxs = [
+            atom.GetIdx() for atom in mol.GetAtoms() if atom.GetAtomicNum() > 1
+        ]
+        np.testing.assert_array_almost_equal(
+            mol.GetConformer().GetPositions(),
+            get_obmol_coords(obmol)[heavy_atom_idxs],
+            decimal=6,
+        )
+
+    # Need to remove Hs for SMILES comparison
+    mol = Chem.rdmolops.RemoveHs(mol, sanitize=True)
+    assert Chem.MolToSmiles(Chem.RemoveHs(mol)) == smi
