1da391604126cfc9f79e0b94cb978e3c1998bfba
Xiaorui Dong <xiaorui@mit.edu>
2023-12-13 20:05:24 -0500

    Add a few more molecular operations to rdtools/mol

--- rdmc/rdtools/mol.py
+++ rdmc/rdtools/mol.py
@@ -1,4 +1,18 @@
+from collections import Counter
+from itertools import product as cartesian_product
+from typing import List, Optional
+
+import numpy as np
+
 from rdkit import Chem
+from rdkit.Geometry.rdGeometry import Point3D
+
+from rdmc.rdtools.atom import get_element_symbol
+from rdmc.rdtools.conf import (
+    embed_multiple_null_confs,
+    reflect as _reflect,
+    set_conformer_coordinates,
+)
 
 
 def get_spin_multiplicity(mol: Chem.Mol) -> int:
@@ -31,6 +45,96 @@ def get_formal_charge(mol: Chem.Mol) -> int:
         int : Formal charge.
     """
     return Chem.GetFormalCharge(mol)
+
+
+def get_heavy_atoms(mol: Chem.Mol) -> int:
+    """
+    Get heavy atoms of a molecule.
+
+    Args:
+        mol (Chem.Mol): The molecule to get heavy atoms.
+
+    Returns:
+        int : Number of heavy atoms.
+    """
+    return [atom for atom in mol.GetAtoms() if atom.GetAtomicNum() != 1]
+
+
+def get_element_symbols(mol: Chem.Mol) -> List[str]:
+    """
+    Get element symbols of a molecule.
+
+    Args:
+        mol (Chem.Mol): The molecule to get element symbols.
+
+    Returns:
+        List[str] : List of element symbols (e.g. ``["H", "C", "O",]`` etc.)
+    """
+    return [get_element_symbol(atom) for atom in mol.GetAtoms()]
+
+
+def get_element_counts(mol: Chem.Mol) -> dict:
+    """
+    Get element counts of a molecule.
+
+    Args:
+        mol (Chem.Mol): The molecule to get element counts.
+
+    Returns:
+        dict: {"element_symbol": count}
+    """
+    return dict(Counter(get_element_symbols(mol)))
+
+
+def combine_mols(
+    mol1: Chem.Mol,
+    mol2: Chem.Mol,
+    offset: Optional[np.ndarray] = None,
+    c_product: bool = False,
+):
+    """
+    Combine the current molecule with the given ``molFrag`` (another molecule
+    or fragment). A new object instance will be created and changes are not made to the current molecule.
+
+    Args:
+        mol1 (Chem.Mol): The current molecule.
+        mol2 (Chem.Mol): The molecule to be combined.
+        offset (np.ndarray, optional): The offset to be added to the coordinates of ``mol2``. It should be a length-3 array.
+                                       This is not used when any of the molecules has 0 conformer. Defaults to ``None``.
+        c_product (bool, optional): If ``True``, generate conformers for every possible combination
+                                    between the current molecule and the ``molFrag``. E.g.,
+                                    (1,1), (1,2), ... (1,n), (2,1), ...(m,1), ... (m,n). :math:`N(conformer) = m \\times n.`
+
+                                    Defaults to ``False``, meaning only generate conformers according to
+                                    (1,1), (2,2), ... When ``c_product`` is set to ``False``, if the current
+                                    molecule has 0 conformer, conformers will be embedded to the current molecule first.
+                                    The number of conformers of the combined molecule will be equal to the number of conformers
+                                    of ``molFrag``. Otherwise, the number of conformers of the combined molecule will be equal
+                                    to the number of conformers of the current molecule. Some coordinates may be filled by 0s,
+                                    if the current molecule and ``molFrag`` have different numbers of conformers.
+
+    Returns:
+        Chem.Mol: The combined molecule.
+    """
+    vector = Point3D()
+    if not c_product and offset is not None:
+        for i, coord in enumerate("xyz"):
+            vector.__setattr__(coord, float(offset[i]))
+
+    combined_mol = Chem.CombineMols(mol1, mol2, vector)
+    if c_product:
+        c1s, c2s = mol1.GetConformers(), mol2.GetConformers()
+        pos_list = [
+            [c1.GetPositions(), c2.GetPositions() + offset]
+            for c1, c2 in cartesian_product(c1s, c2s)
+        ]
+        if len(pos_list) > 0:
+            embed_multiple_null_confs(combined_mol, len(pos_list), random=False)
+            for i, pos in enumerate(pos_list):
+                conf = combined_mol.GetConformer(i)
+                set_conformer_coordinates(conf, np.concatenate(pos))
+
+    return combined_mol
 
 
 def force_no_implicit(mol: Chem.Mol):
---
+++ test/rdtools/test_mol.py
@@ -0,0 +1,116 @@
+import pytest
+
+import numpy as np
+
+from rdmc.rdtools.conf import (
+    add_null_conformer,
+)
+from rdmc.rdtools.conversion import (
+    mol_from_smiles,
+)
+from rdmc.rdtools.mol import (
+    combine_mols,
+    get_element_symbols,
+    get_element_counts,
+)
+from rdmc.rdtools.conf import (
+    embed_multiple_null_confs,
+)
+
+from rdkit import Chem
+
+
+def test_combine_mol_without_conformer():
+    mol1 = Chem.MolFromSmiles("C")
+    mol2 = Chem.MolFromSmiles("CC")
+
+    combined_mol = combine_mols(mol1, mol2)
+    assert combined_mol.GetNumAtoms() == mol1.GetNumAtoms() + mol2.GetNumAtoms()
+
+
+def test_combine_mols_1():
+    mol1 = Chem.MolFromSmiles("CC")
+    mol2 = Chem.MolFromSmiles("C")
+    add_null_conformer(mol1, random=True)
+    add_null_conformer(mol2, random=True)
+
+    pos1 = mol1.GetConformer().GetPositions()
+    pos2 = mol2.GetConformer().GetPositions()
+
+    offset = np.array([1, 0, 0], dtype=float)
+    combined_mol = combine_mols(mol1, mol2, offset=offset, c_product=False)
+
+    assert combined_mol.GetNumAtoms() == mol1.GetNumAtoms() + mol2.GetNumAtoms()
+    assert combined_mol.GetNumConformers() == 1
+    np.testing.assert_allclose(
+        combined_mol.GetConformer().GetPositions(),
+        np.concatenate([pos1, pos2 + offset]),
+    )
+
+
+def test_combine_mols_2():
+    mol1 = Chem.MolFromSmiles("CC")
+    mol2 = Chem.MolFromSmiles("C")
+    n_conf1, n_conf2 = 2, 3
+    embed_multiple_null_confs(mol1, n_conf1, random=True)
+    embed_multiple_null_confs(mol2, n_conf2, random=True)
+
+    offset = np.array([1, 0, 0], dtype=float)
+    combined_mol = combine_mols(mol1, mol2, offset=offset, c_product=True)
+
+    assert combined_mol.GetNumAtoms() == mol1.GetNumAtoms() + mol2.GetNumAtoms()
+    assert combined_mol.GetNumConformers() == n_conf1 * n_conf2
+    for i in range(n_conf1):
+        for j in range(n_conf2):
+            assert np.allclose(
+                combined_mol.GetConformer(i * n_conf2 + j).GetPositions(),
+                np.concatenate(
+                    [
+                        mol1.GetConformer(i).GetPositions(),
+                        mol2.GetConformer(j).GetPositions() + offset,
+                    ]
+                ),
+            )
+
+
+@pytest.mark.parametrize(
+    "smi, expected",
+    [
+        (
+            "[C:2]([H:3])([H:4])([H:5])[H:6].[H:1]",
+            [
+                "H",
+                "C",
+                "H",
+                "H",
+                "H",
+                "H",
+            ],
+        ),
+        (
+            "[O:1][C:2]([C:3]([H:4])[H:5])([H:6])[H:7]",
+            ["O", "C", "C", "H", "H", "H", "H"],
+        ),
+    ],
+)
+def test_get_element_symbols(smi, expected):
+    mol = mol_from_smiles(smi)
+    assert get_element_symbols(mol) == expected
+
+
+@pytest.mark.parametrize(
+    "smi, expected",
+    [
+        (
+            "[C:2]([H:3])([H:4])([H:5])[H:6].[H:1]",
+            {"C": 1, "H": 5},
+        ),
+        (
+            "[O:1][C:2]([C:3]([H:4])[H:5])([H:6])[H:7]",
+            {"C": 2, "H": 4, "O": 1},
+        ),
+    ],
+)
+def test_get_element_counts(smi, expected):
+    mol = mol_from_smiles(smi)
+    assert get_element_counts(mol) == expected
