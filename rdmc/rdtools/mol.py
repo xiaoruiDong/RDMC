6685190886acfd3d876ed8335eedabbf1024dc17
Xiaorui Dong <xiaorui@mit.edu>
2023-12-13 19:50:20 -0500

    Add the missing force_no_implicit and reflect in to rdtools/mol
    Previously some commits have functions using these two functions without they being defined. This commit fixes this.

--- rdmc/rdtools/mol.py
+++ rdmc/rdtools/mol.py
@@ -31,3 +31,26 @@ def get_formal_charge(mol: Chem.Mol) -> int:
         int : Formal charge.
     """
     return Chem.GetFormalCharge(mol)
+
+
+def force_no_implicit(mol: Chem.Mol):
+    """
+    Set no implicit hydrogen for atoms without implicit/explicit hydrogens. When
+    manipulating molecules by changing number of radical electrons / charges and then updating the cached properties,
+    additional hydrogens may be added to the molecule. This function helps avoid this problem.
+    """
+    for atom in mol.GetAtoms():
+        if atom.GetAtomicNum() > 1 and not atom.GetTotalNumHs():
+            atom.SetNoImplicit(True)
+
+
+def reflect(mol: Chem.Mol, conf_id: int = 0):
+    """
+    Reflect the coordinates of the conformer of the molecule.
+
+    Args:
+        mol (Chem.Mol): The molecule to reflect.
+        conf_id (int, optional): The conformer ID to reflect.
+    """
+    conf = mol.GetConformer(conf_id)
+    _reflect(conf)
