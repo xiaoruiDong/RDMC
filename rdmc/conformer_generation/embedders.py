0264362d8b50edb6d25334b7e722df21e63b5176
Xiaorui Dong <xiaorui@mit.edu>
2024-03-14 13:18:20 -0400

    Support use GeoMol embedder in CPU or GPU devices

--- rdmc/conformer_generation/embedders.py
+++ rdmc/conformer_generation/embedders.py
@@ -29,6 +29,7 @@ try:
     from geomol.utils import model_path as geomol_model_path
     import yaml  # only used to load GeoMol parameters
 except ImportError as e:
+    GeoMol = None
     print(e)
     print("No GeoMol installation detected. Skipping import...")
     print("Please install the GeoMol fork at https://github.com/xiaoruiDong/GeoMol")
@@ -164,10 +165,14 @@ class GeoMolEmbedder(ConfGenEmbedder):
         track_stats: bool = False,
         device: str = 'cpu',
     ):
+        if GeoMol is None:
+            raise ImportError("No GeoMol installation detected. Please install the GeoMol fork at https://github.com/xiaoruiDong/GeoMol.")
         super(GeoMolEmbedder, self).__init__(track_stats)
 
         # TODO: add option of pre-pruning geometries using alpha values
         # TODO: investigate option of changing "temperature" each iteration to sample diverse geometries
+        self.device = device
+
         trained_model_dir = geomol_model_path / dataset if trained_model_dir is None else Path(trained_model_dir)
         with open(trained_model_dir / "model_parameters.yml") as f:
             model_parameters = yaml.full_load(f)
@@ -175,13 +180,17 @@ class GeoMolEmbedder(ConfGenEmbedder):
 
         state_dict = torch.load(trained_model_dir / "best_model.pt", map_location=torch.device(device))
         model.load_state_dict(state_dict, strict=True)
-        model.to(device)
+        model.to(self.device)
         model.eval()
         self.model = model
         self.tg_data = None
         self.std = model_parameters["hyperparams"]["random_vec_std"]
         self.temp_schedule = temp_schedule
         self.dataset = dataset
+
+    def to(self, device: str):
+        self.device = device
+        self.model.to(device)
 
     def embed_conformers(
         self,
