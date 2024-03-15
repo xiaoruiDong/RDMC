721dd40075dbe51d6857b8b09333d0a24419f2f9
Xiaorui Dong <xiaorui@mit.edu>
2024-03-15 15:10:51 -0400

    Add a helper function to prepare the input for freq_viewer

--- rdmc/rdtools/view/freq.py
+++ rdmc/rdtools/view/freq.py
@@ -39,3 +39,33 @@ def freq_viewer(
         default_arrow_spec,
     )
     return viewer
+
+
+def merge_xyz_dxdydz(
+    xyz: str,
+    dxdydz: list,
+) -> str:
+    """
+    A helper function to create input for freq_viewer.
+    Merge the xyz string with the dxdydz information.
+
+    Args:
+        xyz (str): The xyz string.
+        dxdydz (list): The dx dy dz in a 3 x N matrix like list or array.
+
+    Returns:
+        str: The xyz string with dxdydz information.
+    """
+    lines = xyz.strip().splitlines()
+
+    assert len(lines) - 2 == len(dxdydz), (
+        f"The number of atoms doesn't match xyz ({len(lines) - 2}) "
+        f"and dxdydz ({len(dxdydz)})"
+    )
+
+    new_lines = lines[0:2] + [
+        line.strip() + "\t\t" + "".join([f"{dx:<12}" for dx in dxdydz_onerow])
+        for line, dxdydz_onerow in zip(lines[2:], dxdydz)
+    ]
+
+    return "\n".join(new_lines)
