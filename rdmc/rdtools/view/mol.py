1678ed35616d9d3635bbd34980bce9c4a569a937
Xiaorui Dong <xiaorui@mit.edu>
2024-03-15 11:24:14 -0400

    Migrate view to rdtools: add mol and conformer animation

--- rdmc/rdtools/view/mol.py
+++ rdmc/rdtools/view/mol.py
@@ -1,6 +1,8 @@
+from typing import List, Optional
 import logging
+
 from rdkit import Chem
-from rdmc.rdtools.view.base import base_viewer
+from rdmc.rdtools.view.base import base_viewer, animation_viewer
 
 logger = logging.getLogger(__name__)
 
@@ -29,3 +31,34 @@ def mol_viewer(
         obj, model = Chem.MolToXYZBlock(mol, confId=conf_id), 'xyz'
 
     return base_viewer(obj, model, **kwargs)
+
+
+def mol_animation(
+    mols: List["Mol"],
+    conf_ids: List[int] = None,
+    interval: int = 1000,  # more proper to look at different molecules
+    **kwargs,
+) -> "py3Dmol.view":
+    """
+    Create an animation viewer for viewing the RDKit Molecules. This
+    viewer accepts additional keyword arguments for viewer specs,
+    following the same way animation_viewer is implemented.
+
+    Args:
+        mols (list): A list of RDKit Molecules, each assumes to contain at least one conformer.
+        conf_ids (list, optional): A list of IDs of the conformers to view. If ``None``, the first conformer of
+                         each molecule will be displayed. Defaults to None.
+        interval (int, optional): The time interval between each frame in millisecond. Defaults to ``1000``.
+
+    Returns:
+        py3Dmol.view: The viewer.
+    """
+    conf_ids = conf_ids or [0] * len(mols)
+
+    assert len(mols) == len(conf_ids), \
+        f"The number of molecules ({len(mols)}) and the " \
+        f"number of conformers ({len(conf_ids)} must be the same."
+
+    xyz = "".join([Chem.MolToXYZBlock(mol, confId=i) for mol, i in zip(mols, conf_ids)])
+
+    return animation_viewer(xyz, "xyz", interval=interval, **kwargs)
