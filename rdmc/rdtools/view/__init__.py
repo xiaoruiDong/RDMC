529add1c3871199784e6b3895cbab6716654f2c0
Xiaorui Dong <xiaorui@mit.edu>
2024-03-15 17:30:26 -0400

    Make major viewers directly exposed at rdtools.view

---
+++ rdmc/rdtools/view/__init__.py
@@ -0,0 +1,7 @@
+from rdmc.rdtools.view.base import grid_viewer
+from rdmc.rdtools.view.mol import mol_viewer, mol_animation, ts_viewer
+from rdmc.rdtools.view.freq import freq_viewer
+from rdmc.rdtools.view.conf import conformer_animation, conformer_viewer
+from rdmc.rdtools.view.interact import interactive_conformer_viewer
+from rdmc.rdtools.view.reaction import reaction_viewer
+from rdmc.rdtools.view.utils import merge_xyz_dxdydz
