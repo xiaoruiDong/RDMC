c1a7297aced5e98860bbc3cc889f8d52ccbe299c
Xiaorui Dong <xiaorui@mit.edu>
2024-03-17 22:01:14 -0400

    Migrate featurizer to rdtools: clean featurizer module
    1. Remove redundant name of the fingerprint dict; 2. drop the support to RDKitMol; 3. add a way to assign data type

--- rdmc/featurizer.py
+++ rdmc/featurizer.py
@@ -8,29 +8,30 @@ This module contains functions for generating molecular fingerprints.
 import numpy as np
 
 from rdkit.Chem import Mol
-from rdkit.Chem.rdFingerprintGenerator import (GetAtomPairGenerator,
-                                               GetMorganGenerator,
-                                               GetRDKitFPGenerator,
-                                               GetTopologicalTorsionGenerator)
+from rdkit.Chem.rdFingerprintGenerator import (
+    GetAtomPairGenerator,
+    GetMorganGenerator,
+    GetRDKitFPGenerator,
+    GetTopologicalTorsionGenerator
+)
 
 
-fingerprint_dicts = {'atompair': GetAtomPairGenerator,
-                     'atompairfp': GetAtomPairGenerator,
-                     'morgan': GetMorganGenerator,
-                     'morganfp': GetMorganGenerator,
-                     'rdkit': GetRDKitFPGenerator,
-                     'rdkitfp': GetRDKitFPGenerator,
-                     'topologicaltorsion': GetTopologicalTorsionGenerator,
-                     'topologicaltorsionfp': GetTopologicalTorsionGenerator,
-                     }
+fingerprint_dicts = {
+    'atom_pair': GetAtomPairGenerator,
+    'morgan': GetMorganGenerator,
+    'rdkit': GetRDKitFPGenerator,
+    'topological_torsion': GetTopologicalTorsionGenerator,
+}
 
 
-def get_fingerprint(mol: 'RDKitMol',
-                    count: bool = False,
-                    fp_type: str = 'morgan',
-                    num_bits: int = 2048,
-                    **kwargs,
-                    ) -> np.ndarray:
+def get_fingerprint(
+    mol: Mol,
+    count: bool = False,
+    fp_type: str = 'morgan',
+    num_bits: int = 2048,
+    dtype: str = 'int32',
+    **kwargs,
+) -> np.ndarray:
     """
     A helper function for generating molecular fingerprints. Please visit
     `RDKit <https://www.rdkit.org/docs/source/rdkit.Chem.rdFingerprintGenerator.html>`_ for
@@ -42,18 +43,18 @@ def get_fingerprint(mol: 'RDKitMol',
         mol: The molecule to generate a fingerprint for.
         count: Whether to generate a count fingerprint. Default is ``False``.
         fp_type: The type of fingerprint to generate. Options are:
-                 ``'atompair'``, ``'morgan'``, ``'rdkit'``,
-                 and ``'topologicaltorsion'``.
+                 ``'atom_pair'``, ``'morgan'`` (default), ``'rdkit'``,
+                 and ``'topological_torsion'``.
         num_bits: The length of the fingerprint. Default is ``2048``.
 
     Returns:
         np.ndarray: A numpy array of the molecular fingerprint.
     """
-    if not isinstance(mol, Mol):  # Convert RDKitMol
-        mol = mol.ToRWMol()
-
-    generator = fingerprint_dicts[fp_type.lower()](fpSize=num_bits,
-                                                   **kwargs)
-    return getattr(generator,
-                   f'Get{"Count" if count else ""}FingerprintAsNumPy'
-                   )(mol)
+    generator = fingerprint_dicts[fp_type.lower()](
+        fpSize=num_bits,
+        **kwargs,
+    )
+    return getattr(
+        generator,
+        f'Get{"Count" if count else ""}FingerprintAsNumPy'
+    )(mol).astype(dtype)
