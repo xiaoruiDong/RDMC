3f1895413c1e3b17173beeddc1c22371b1195275
Xiaorui Dong <xiaorui@mit.edu>
2024-03-22 17:14:30 -0400

    Fix the fake package module
    The previous implementation causes  recursion error, given calling self.something inside __getattribute__

--- rdtools/utils.py
+++ rdtools/utils.py
@@ -1,23 +1,34 @@
 from typing import Optional
 
+FAKE_PACKAGES = set()
+
 
 class FakeModule:
 
     def __init__(self, module_name: str, package_name: Optional[str] = None):
-        package_name = package_name or module_name
-        self.module_name = module_name
-        self.package_name = package_name
+
+        object.__setattr__(self, "module_name", module_name)
+        object.__setattr__(self, "package_name", package_name or module_name)
+        FAKE_PACKAGES.add(package_name or module_name)
 
     def __getattribute__(self, __name: str):
+
+        module_name = object.__getattribute__(self, "module_name")
+        package_name = object.__getattribute__(self, "package_name")
+
         raise RuntimeError(
-            f"The method ({self.module_name}.{__name}) cannot be run as {self.package_name} is not installed. "
-            f"Please install the {self.package_name} and restart the python Kernel."
+            f"The method ({module_name}.{__name}) cannot be run as {package_name} is not installed. "
+            f"Please install the {package_name} and restart the python Kernel."
         )
 
     def __call__(self, *args, **kwargs):
+
+        module_name = object.__getattribute__(self, "module_name")
+        package_name = object.__getattribute__(self, "package_name")
+
         raise RuntimeError(
-            f"The function ({self.module_name}) cannot be run as {self.package_name} is not installed. "
-            f"Please install the {self.package_name} and restart the python Kernel."
+            f"The method ({module_name}()) cannot be run as {package_name} is not installed. "
+            f"Please install the {package_name} and restart the python Kernel."
         )
 
 
