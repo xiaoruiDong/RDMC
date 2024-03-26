921fc0caa1a4172cd9fb670ccfd67ee43dcc941e
Xiaorui Dong <xiaorui@mit.edu>
2024-03-26 14:07:56 -0400

    Update comp_env.ase
    fix one typo; add Atoms

--- rdmc/conformer_generation/comp_env/ase.py
+++ rdmc/conformer_generation/comp_env/ase.py
@@ -4,10 +4,11 @@ from rdmc.conformer_generation.comp_env.software import try_import
 package_name = "ase"
 namespace = globals()
 modules = [
+    "ase.Atoms",
     "ase.autoneb.AutoNEB",
     "ase.calculators.orca.ORCA",
     "ase.calculators.calculator.Calculator",
-    "ase.calculators.calculator.CalculatioFailed",
+    "ase.calculators.calculator.CalculationFailed",
     "ase.optimize.QuasiNewton",
 ]
 
