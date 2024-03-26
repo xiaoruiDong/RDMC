ce4fdb878f8e3d1d2076739df8dad25ee38c5f25
Xiaorui Dong <xiaorui@mit.edu>
2024-03-26 12:18:58 -0400

    Add binary check and auto import to comp_env.xtb

--- rdmc/conformer_generation/comp_env/xtb/__init__.py
+++ rdmc/conformer_generation/comp_env/xtb/__init__.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from rdmc.conformer_generation.comp_env.software import register_binary, try_import
+
+register_binary("xtb")
+register_binary("crest")
+
+try_import("xtb.ase.calculator.XTB", "xtb_calculator", globals(), "xtb-python")
