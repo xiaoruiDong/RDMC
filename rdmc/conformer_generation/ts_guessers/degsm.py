e01c84fc1feb2e2bddc657bc55f6d808bde3bff5
Xiaorui Dong <xiaorui@mit.edu>
2024-03-26 18:50:11 -0400

    Update ts_guessers.degsm

--- rdmc/conformer_generation/ts_guessers/degsm.py
+++ rdmc/conformer_generation/ts_guessers/degsm.py
@@ -1,6 +1,4 @@
-import os
 import subprocess
-import tempfile
 from typing import Optional
 
 import numpy as np
@@ -9,6 +7,7 @@ import numpy as np
 from rdmc.conformer_generation.ts_guessers.base import TSInitialGuesser
 from rdmc.external.inpwriter import write_gaussian_gsm
 from rdmc.conformer_generation.comp_env import gsm_available
+from rdmc.conformer_generation.comp_env.software import get_binary
 
 
 class DEGSMGuesser(TSInitialGuesser):
@@ -18,8 +17,6 @@ class DEGSMGuesser(TSInitialGuesser):
     Args:
         track_stats (bool, optional): Whether to track the status. Defaults to ``False``.
     """
-
-    _avail = gsm_available
 
     def __init__(
         self,
@@ -35,22 +32,25 @@ class DEGSMGuesser(TSInitialGuesser):
         Args:
             track_stats (bool, optional): Whether to track the status. Defaults to ``False``.
         """
-        super(DEGSMGuesser, self).__init__(track_stats)
+        super().__init__(track_stats)
         self.gsm_args = gsm_args
         self.method = method
         self.nprocs = nprocs
         self.memory = memory
 
-        try:
-            self.gsm_entry_point = os.environ["gsm"]
-        except KeyError:
-            raise RuntimeError("No GSM entry point is found in the PATH.")
+    def is_available(self) -> bool:
+        """
+        Check if the DE-GSM method is available.
 
-    def generate_ts_guesses(
+        Returns:
+            bool: ``True`` if the DE-GSM method is available, ``False`` otherwise.
+        """
+        return gsm_available
+
+    def run(
         self,
         mols: list,
         multiplicity: Optional[int] = None,
-        save_dir: Optional[str] = None,
     ):
         """
         Generate TS guesser.
@@ -58,7 +58,6 @@ class DEGSMGuesser(TSInitialGuesser):
         Args:
             mols (list): A list of reactant and product pairs.
             multiplicity (int, optional): The spin multiplicity of the reaction. Defaults to ``None``.
-            save_dir (Optional[str], optional): The path to save the results. Defaults to ``None``.
 
         Returns:
             RDKitMol: The TS molecule in RDKitMol with 3D conformer saved with the molecule.
@@ -67,65 +66,58 @@ class DEGSMGuesser(TSInitialGuesser):
         # currently use the save directory as the working directory
         # This may not be ideal for some QM software, and whether to add a support
         # for scratch directory is left for future decision
-        work_dir = os.path.abspath(save_dir) if save_dir else tempfile.mkdtemp()
-        lot_inp_file = os.path.join(work_dir, "qstart.inp")
+
+        lot_inp_file = self.work_dir / "qstart.inp"
         lot_inp_str = write_gaussian_gsm(self.method, self.memory, self.nprocs)
         with open(lot_inp_file, "w") as f:
             f.writelines(lot_inp_str)
 
-        ts_guesses, used_rp_combos = [], []
+        ts_guesses, used_rp_combos = {}, []
         for i, (r_mol, p_mol) in enumerate(mols):
 
             # TODO: Need to clean the logic here, `ts_conf_dir` is used no matter `save_dir` being true
-            ts_conf_dir = os.path.join(work_dir, f"degsm_conf{i}")
-            if not os.path.exists(ts_conf_dir):
-                os.makedirs(ts_conf_dir)
+            ts_conf_dir = self.work_dir / f"degsm_conf{i}"
+            ts_conf_dir.mkdir(parents=True, exist_ok=True)
 
-            r_xyz = r_mol.ToXYZ()
-            p_xyz = p_mol.ToXYZ()
-
-            xyz_file = os.path.join(ts_conf_dir, f"degsm_conf{i}.xyz")
+            xyz_file = ts_conf_dir / f"degsm_conf{i}.xyz"
             with open(xyz_file, "w") as f:
-                f.write(r_xyz)
-                f.write(p_xyz)
+                f.write(r_mol.ToXYZ())
+                f.write(p_mol.ToXYZ())
+            used_rp_combos.append((r_mol, p_mol))
 
             try:
-                command = f"{self.gsm_entry_point} -xyzfile {xyz_file} -nproc {self.nprocs} -multiplicity {multiplicity} -mode DE_GSM -package Gaussian -lot_inp_file {lot_inp_file} {self.gsm_args}"
-                with open(os.path.join(ts_conf_dir, "degsm.log"), "w") as f:
-                    gsm_run = subprocess.run(
+                command = f"{get_binary('gsm')} -xyzfile {xyz_file} -nproc {self.nprocs} -multiplicity {multiplicity} -mode DE_GSM -package Gaussian -lot_inp_file {lot_inp_file} {self.gsm_args}"
+                with open(ts_conf_dir / "degsm.log", "w") as f:
+                    subprocess.run(
                         [command],
                         stdout=f,
                         stderr=subprocess.STDOUT,
                         cwd=ts_conf_dir,
                         shell=True,
                     )
-                used_rp_combos.append((r_mol, p_mol))
-                tsnode_path = os.path.join(ts_conf_dir, "TSnode_0.xyz")
+
+                tsnode_path = ts_conf_dir / "TSnode_0.xyz"
                 with open(tsnode_path) as f:
                     positions = f.read().splitlines()[2:]
                     positions = np.array(
                         [line.split()[1:] for line in positions], dtype=float
                     )
-                ts_guesses.append(positions)
+                ts_guesses[i] = positions
             except FileNotFoundError:
-                pass
-
-        if len(ts_guesses) == 0:
-            return None
+                ts_guesses[i] = None
 
         # copy data to mol
         ts_mol = mols[0][0].Copy(quickCopy=True)
         ts_mol.EmbedMultipleNullConfs(len(ts_guesses))
         [
             ts_mol.GetEditableConformer(i).SetPositions(p)
-            for i, p in enumerate(ts_guesses)
+            for i, p in ts_guesses.items()
+            if ts_guesses is not None
         ]
 
-        if save_dir:
-            self.save_guesses(save_dir, used_rp_combos, ts_mol)
+        if self.save_dir:
+            self.save_guesses(used_rp_combos, ts_mol)
 
-        ts_mol.KeepIDs = {
-            i: True for i in range(ts_mol.GetNumConformers())
-        }  # todo: need to change
+        ts_mol.KeepIDs = {i: val is not None for i, val in ts_guesses.items()}
 
         return ts_mol
