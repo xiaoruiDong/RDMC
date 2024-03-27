00d127803a7aaf1354b0ab38b905cf4745f82e5d
Xiaorui Dong <xiaorui@mit.edu>
2024-03-27 09:53:08 -0400

    Simplify the implementation of base verifiers and xtb verifiers
    1. n_failures, percent_failures, opt_cycles are not used at all. They are only used in optimizers now
    2. rename verify* to run
    3. inherit __call__ from parent
    4. add allowed_num_neg_freqs as a class method to xtb frequency verifier

--- rdmc/conformer_generation/ts_verifiers/xtb.py
+++ rdmc/conformer_generation/ts_verifiers/xtb.py
@@ -1,15 +1,9 @@
-import os
-import pickle
-from typing import Optional
+from rdmc.conformer_generation.verifiers.xtb import (
+    XTBFrequencyVerifier as XTBFreqVerifier,
+)
 
 
-from rdmc.conformer_generation.ts_verifiers.base import TSVerifier
-
-from rdmc.conformer_generation.comp_env.xtb.opt import run_xtb_calc
-from rdmc.conformer_generation.comp_env import xtb_available
-
-
-class XTBFrequencyVerifier(TSVerifier):
+class XTBFrequencyVerifier(XTBFreqVerifier):
     """
     The class for verifying the TS by calculating and checking its frequencies using XTB.
 
@@ -20,56 +14,16 @@ class XTBFrequencyVerifier(TSVerifier):
         track_stats (bool, optional): Whether to track stats. Defaults to ``False``.
     """
 
-    _avail = xtb_available
+    allowed_num_neg_freqs = 1
 
-    def __init__(self, cutoff_frequency: float = -100.0, track_stats: bool = False):
+    def __init__(self, cutoff_frequency: float = -10.0, track_stats: bool = False):
         """
         Initiate the XTB frequency verifier.
 
         Args:
             cutoff_frequency (float, optional): Cutoff frequency above which a frequency does not correspond to a TS
                                                 imaginary frequency to avoid small magnitude frequencies which correspond to internal bond rotations
-                                                Defaults to ``-100.`` cm-1
+                                                Defaults to ``-10.`` cm-1
             track_stats (bool, optional): Whether to track stats. Defaults to ``False``.
         """
-        super(XTBFrequencyVerifier, self).__init__(track_stats)
-
-        self.cutoff_frequency = cutoff_frequency
-
-    def verify_ts_guesses(
-        self,
-        ts_mol: "RDKitMol",
-        multiplicity: int = 1,
-        save_dir: Optional[str] = None,
-        **kwargs,
-    ) -> "RDKitMol":
-        """
-        Verifying TS guesses (or optimized TS geometries).
-
-        Args:
-            ts_mol ('RDKitMol'): The TS in RDKitMol object with 3D geometries embedded.
-            multiplicity (int, optional): The spin multiplicity of the TS. Defaults to ``1``.
-            save_dir (str, optional): The directory path to save the results. Defaults to ``None``.
-
-        Returns:
-            RDKitMol: The molecule in RDKitMol object with verification results stored in ``KeepIDs``.
-        """
-        for i in range(ts_mol.GetNumConformers()):
-            if ts_mol.KeepIDs[i]:
-                if ts_mol.frequency[i] is None:
-                    props = run_xtb_calc(
-                        ts_mol, confId=i, job="--hess", uhf=multiplicity - 1
-                    )
-                    frequencies = props["frequencies"]
-                else:
-                    frequencies = ts_mol.frequency[i]
-
-                # Check if the number of large negative frequencies is equal to 1
-                freq_check = sum(frequencies < self.cutoff_frequency) == 1
-                ts_mol.KeepIDs[i] = freq_check
-
-        if save_dir:
-            with open(os.path.join(save_dir, "freq_check_ids.pkl"), "wb") as f:
-                pickle.dump(ts_mol.KeepIDs, f)
-
-        return ts_mol
+        super().__init__(cutoff_frequency, track_stats)
--- rdmc/conformer_generation/verifiers/xtb.py
+++ rdmc/conformer_generation/verifiers/xtb.py
@@ -1,13 +1,12 @@
 import os
 import pickle
-from typing import Optional
 
 from rdmc.conformer_generation.verifiers.base import Verifier
 from rdmc.conformer_generation.comp_env.xtb.opt import run_xtb_calc
-from rdmc.conformer_generation.comp_env import xtb_available
+from rdmc.conformer_generation.task.xtb import XTBTask
 
 
-class XTBFrequencyVerifier(Verifier):
+class XTBFrequencyVerifier(XTBTask, Verifier):
     """
     The class for verifying the stable species by calculating and checking its frequencies using XTB.
 
@@ -18,27 +17,24 @@ class XTBFrequencyVerifier(Verifier):
         track_stats (bool, optional): Whether to track stats. Defaults to ``False``.
     """
 
-    _avail = xtb_available
+    allowed_num_neg_freqs = 0
 
-    def __init__(self, cutoff_frequency: float = -100.0, track_stats: bool = False):
+    def __init__(self, cutoff_frequency: float = 0.0, track_stats: bool = False):
         """
         Initiate the XTB frequency verifier.
 
         Args:
-            cutoff_frequency (float, optional): Cutoff frequency above which a frequency does not correspond to a TS
-                                                imaginary frequency to avoid small magnitude frequencies which correspond to internal bond rotations
-                                                Defaults to ``-100.`` cm-1.
+            cutoff_frequency (float, optional): Cutoff frequency used to determine if the molecules has an imaginary frequency mode.
+                Defaults to ``0.0`` cm-1.
             track_stats (bool, optional): Whether to track stats. Defaults to ``False``.
         """
-        super(XTBFrequencyVerifier, self).__init__(track_stats)
-
+        super().__init__(track_stats)
         self.cutoff_frequency = cutoff_frequency
 
-    def verify_guesses(
+    def run(
         self,
         mol: "RDKitMol",
         multiplicity: int = 1,
-        save_dir: Optional[str] = None,
         **kwargs,
     ):
         """
@@ -63,11 +59,14 @@ class XTBFrequencyVerifier(Verifier):
                     else:
                         frequencies = mol.frequency[i]
 
-                    freq_check = sum(frequencies < self.cutoff_frequency) == 0
+                    freq_check = (
+                        sum(frequencies < self.cutoff_frequency)
+                        == self.allowed_num_neg_freqs
+                    )
                     mol.KeepIDs[i] = freq_check
 
-        if save_dir:
-            with open(os.path.join(save_dir, "freq_check_ids.pkl"), "wb") as f:
+        if self.save_dir:
+            with open(os.path.join(self.save_dir, "freq_check_ids.pkl"), "wb") as f:
                 pickle.dump(mol.KeepIDs, f)
 
         return mol
