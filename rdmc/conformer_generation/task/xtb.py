5d2cfe83e2916e5f6f952894ee907d4a5c84d4af
Xiaorui Dong <xiaorui@mit.edu>
2024-03-27 11:06:01 -0400

    Update the xtb frequency verifiers

--- rdmc/conformer_generation/task/xtb.py
+++ rdmc/conformer_generation/task/xtb.py
@@ -5,8 +5,5 @@ from rdmc.conformer_generation.task.basetask import BaseTask
 
 class XTBTask(BaseTask):
 
-    def __init__(self, track_stats: bool = False):
-        super().__init__(track_stats)
-
     def is_available(self):
         return xtb_available
--- rdmc/conformer_generation/ts_verifiers/xtb.py
+++ rdmc/conformer_generation/ts_verifiers/xtb.py
@@ -1,29 +1,19 @@
+from rdmc.conformer_generation.ts_verifiers.base import TSFreqVerifier
 from rdmc.conformer_generation.verifiers.xtb import (
     XTBFrequencyVerifier as XTBFreqVerifier,
 )
 
 
-class XTBFrequencyVerifier(XTBFreqVerifier):
+class XTBFrequencyVerifier(
+    XTBFreqVerifier,
+    TSFreqVerifier,
+):
     """
     The class for verifying the TS by calculating and checking its frequencies using XTB.
 
     Args:
         cutoff_frequency (float, optional): Cutoff frequency above which a frequency does not correspond to a TS
                                             imaginary frequency to avoid small magnitude frequencies which correspond to internal bond rotations
-                                            Defaults to ``-100.`` cm-1
+                                            Defaults to ``-10.`` cm-1
         track_stats (bool, optional): Whether to track stats. Defaults to ``False``.
     """
-
-    allowed_num_neg_freqs = 1
-
-    def __init__(self, cutoff_frequency: float = -10.0, track_stats: bool = False):
-        """
-        Initiate the XTB frequency verifier.
-
-        Args:
-            cutoff_frequency (float, optional): Cutoff frequency above which a frequency does not correspond to a TS
-                                                imaginary frequency to avoid small magnitude frequencies which correspond to internal bond rotations
-                                                Defaults to ``-10.`` cm-1
-            track_stats (bool, optional): Whether to track stats. Defaults to ``False``.
-        """
-        super().__init__(cutoff_frequency, track_stats)
