42aeae69bcf493ca6fa904d98e2439366ae1fd61
Xiaorui Dong <xiaorui@mit.edu>
2024-03-29 00:56:07 -0400

    Update the unit test of embedder
    Embedder now returns a RDKitMol instead of mol_data

--- test/conformer_generation/test_embedder.py
+++ test/conformer_generation/test_embedder.py
@@ -15,7 +15,7 @@ def etkdg_embedder():
         "[C:1]([C@@:2]([O:3][H:12])([C:4]([N:5]([C:6](=[O:7])[H:16])"
         "[H:15])([H:13])[H:14])[H:11])([H:8])([H:9])[H:10]",
         "CN1C2=C(C=C(C=C2)Cl)C(=NCC1=O)C3=CC=CC=C3",
-    ]
+    ],
 )
 @pytest.mark.parametrize(
     "n_conf",
@@ -23,4 +23,4 @@ def etkdg_embedder():
 )
 def test_embedding_etkdg(etkdg_embedder, smi, n_conf):
 
-    assert len(etkdg_embedder(smi, n_conf)) == n_conf
+    assert etkdg_embedder(smi, n_conf).GetNumConformers() == n_conf
