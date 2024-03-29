16323322598444cc2d8d62a61eb5d300753e7735
Xiaorui Dong <xiaorui@mit.edu>
2024-03-29 10:40:07 -0400

    Bugfix: import torsional-diffusion embedder in embedder.__init__

--- rdmc/conformer_generation/embedders/__init__.py
+++ rdmc/conformer_generation/embedders/__init__.py
@@ -1,3 +1,6 @@
 from rdmc.conformer_generation.embedders.etkdg import ETKDGEmbedder
 from rdmc.conformer_generation.embedders.geomol import GeoMolEmbedder
 from rdmc.conformer_generation.embedders.random import RandomEmbedder
+from rdmc.conformer_generation.embedders.torsional_diffusion import (
+    TorsionalDiffusionEmbedder,
+)
