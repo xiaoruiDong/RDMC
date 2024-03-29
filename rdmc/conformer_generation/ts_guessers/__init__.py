8384370089d1a633d3260b7f318655bb9e0d3415
Xiaorui Dong <xiaorui@mit.edu>
2024-03-29 00:32:51 -0400

    Add an attempt to add OA-ReactDiff to ts guessers

--- rdmc/conformer_generation/ts_guessers/__init__.py
+++ rdmc/conformer_generation/ts_guessers/__init__.py
@@ -3,3 +3,4 @@ from rdmc.conformer_generation.ts_guessers.tsegnn import TSEGNNGuesser
 from rdmc.conformer_generation.ts_guessers.tsgcn import TSGCNGuesser
 from rdmc.conformer_generation.ts_guessers.rmsdpp import RMSDPPGuesser
 from rdmc.conformer_generation.ts_guessers.degsm import DEGSMGuesser
+from rdmc.conformer_generation.ts_guessers.oa_reactdiff import OAReactDiffGuesser
