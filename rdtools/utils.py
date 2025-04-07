c22738b61b9c331dd579da4ad5dd2cf25164e425
Xiaorui Dong <dxr1234@gmail.com>
2025-04-06 23:01:23 -0400

    style: fix rdtools.utils

--- rdtools/utils.py
+++ rdtools/utils.py
@@ -1,18 +1,35 @@
-from typing import Optional
+"""Utils used in the RDTools package."""
+
+from typing import Any, Optional
 
 FAKE_PACKAGES = set()
 
 
 class FakeModule:
+    """Fake module that raises an error when used.
+
+    This is to allow users to import modules that are not installed, but run into
+    an error when they try to use them. This is useful for optional dependencies.
+
+    Args:
+        module_name (str): The name of the module.
+        package_name (Optional[str], optional): The name of the package. Defaults to None.
+    """
 
     def __init__(self, module_name: str, package_name: Optional[str] = None):
-
         object.__setattr__(self, "module_name", module_name)
         object.__setattr__(self, "package_name", package_name or module_name)
         FAKE_PACKAGES.add(package_name or module_name)
 
-    def __getattribute__(self, __name: str):
+    def __getattribute__(self, __name: str) -> None:
+        """Get the attribute of the module.
 
+        Args:
+            __name (str): The name of the attribute.
+
+        Raises:
+            RuntimeError: If the module is not installed.
+        """
         module_name = object.__getattribute__(self, "module_name")
         package_name = object.__getattribute__(self, "package_name")
 
@@ -21,8 +38,16 @@ class FakeModule:
             f"Please install the {package_name} and restart the python Kernel."
         )
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args: Any, **kwargs: Any) -> None:
+        """Call the module.
 
+        Args:
+            *args (Any): The arguments to the module.
+            **kwargs (Any): The keyword arguments to the module.
+
+        Raises:
+            RuntimeError: If the module is not installed.
+        """
         module_name = object.__getattribute__(self, "module_name")
         package_name = object.__getattribute__(self, "package_name")
 
@@ -32,8 +57,14 @@ class FakeModule:
         )
 
 
-def get_fake_module(module_name: str, package_name: Optional[str] = None):
-    """
-    Returns a fake module that raises an error when used.
+def get_fake_module(module_name: str, package_name: Optional[str] = None) -> FakeModule:
+    """Returns a fake module that raises an error when used.
+
+    Args:
+        module_name (str): The name of the module.
+        package_name (Optional[str], optional): The name of the package. Defaults to None.
+
+    Returns:
+        FakeModule: The fake module.
     """
     return FakeModule(module_name, package_name)
