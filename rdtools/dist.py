21c968909517d89136d3605cf504757db68a0df2
Xiaorui Dong <dxr1234@gmail.com>
2025-04-07 00:17:55 -0400

    style: fix rdtools.dist

--- rdtools/dist.py
+++ rdtools/dist.py
@@ -1,22 +1,25 @@
+# -*- coding: utf-8 -*-
+"""A module contains functions to manipulate distances in a molecule."""
+
 from functools import lru_cache
-from typing import List, Tuple
+from typing import Literal
 
 import numpy as np
-
+import numpy.typing as npt
 from rdkit import Chem
-from rdtools.element import get_vdw_radius, get_covalent_radius, get_bond_radius
+
+from rdtools.element import get_bond_radius, get_covalent_radius, get_vdw_radius
 
 
-def get_adjacency_matrix(mol: Chem.Mol) -> np.ndarray:
-    """
-    Get the adjacency matrix of the molecule.
+def get_adjacency_matrix(mol: Chem.Mol) -> npt.NDArray[np.int_]:
+    """Get the adjacency matrix of the molecule.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule to get the adjacency matrix of.
+        mol (Chem.Mol): The molecule to get the adjacency matrix of.
 
     Returns:
-        numpy.ndarray: A square adjacency matrix of the molecule, where a `1` indicates that atoms are bonded
-                        and a `0` indicates that atoms aren't bonded.
+        npt.NDArray[np.int_]: A square adjacency matrix of the molecule, where a `1` indicates that atoms are bonded
+            and a `0` indicates that atoms aren't bonded.
     """
     return Chem.GetAdjacencyMatrix(mol)
 
@@ -25,30 +28,32 @@ def get_distance_matrix(
     mol: Chem.Mol,
     conf_id: int = -1,
     balaban: bool = False,
-) -> np.ndarray:
-    """
-    Get the distance matrix of the molecule.
+) -> npt.NDArray[np.float64]:
+    """Get the distance matrix of the molecule.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule to get the distance matrix of.
+        mol (Chem.Mol): The molecule to get the distance matrix of.
         conf_id (int, optional): The conformer ID of the molecule to get the distance matrix of. Defaults to ``-1``.
+        balaban (bool, optional): Whether to use the Balaban distance. Defaults to ``False``.
+            If ``True``, the distance matrix will be calculated using the Balaban distance.
 
     Returns:
-        numpy.ndarray: A square distance matrix of the molecule, where the value of each element is the distance
-                        between the atoms.
+        npt.NDArray[np.float64]: A square distance matrix of the molecule, where the value of each element is the distance
+            between the atoms.
     """
     return Chem.Get3DDistanceMatrix(mol, confId=conf_id, useAtomWts=balaban)
 
 
-def _create_matrix_with_radii_values(radii: np.array) -> np.array:
-    """
-    Create a matrix with the radii values of the atoms.
+def _create_matrix_with_radii_values(
+    radii: npt.NDArray[np.float64],
+) -> npt.NDArray[np.float64]:
+    """Create a matrix with the radii values of the atoms.
 
     Args:
-        radii (numpy.ndarray): A 1 x N array of the radii of the atoms.
+        radii (npt.NDArray[np.float64]): A 1 x N array of the radii of the atoms.
 
     Returns:
-        numpy.ndarray: A N x N square matrix with the radii values of the atoms.
+        npt.NDArray[np.float64]: A N x N square matrix with the radii values of the atoms.
     """
     num_atom = len(radii)
     mat = np.zeros((num_atom, num_atom))
@@ -58,71 +63,63 @@ def _create_matrix_with_radii_values(radii: np.array) -> np.array:
 @lru_cache()
 def get_vdw_distance_matrix(
     mol: Chem.Mol,
-) -> np.array:
-    """
-    Generate a van der Waals matrix of the molecule.
+) -> npt.NDArray[np.float64]:
+    """Generate a van der Waals matrix of the molecule.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule to generate the derived van der Waals matrix of.
+        mol (Chem.Mol): The molecule to generate the derived van der Waals matrix of.
 
-    Raises:
-        ValueError: Invalid threshold is supplied.
+    Returns:
+        npt.NDArray[np.float64]: A square van der Waals matrix of the molecule, where the value of each element is the van der
+            Waals radius of the atoms.
     """
-    vdw_radii = np.array(
+    vdw_radii = np.array([
         [
-            [
-                get_vdw_radius(mol.GetAtomWithIdx(i).GetAtomicNum())
-                for i in range(mol.GetNumAtoms())
-                # TODO: Update this to mol.GetAtoms() when RDKit 2023.09 is widely used
-            ]
+            get_vdw_radius(mol.GetAtomWithIdx(i).GetAtomicNum())
+            for i in range(mol.GetNumAtoms())
+            # TODO: Update this to mol.GetAtoms() when RDKit 2023.09 is widely used
         ]
-    )
+    ])
     return _create_matrix_with_radii_values(vdw_radii)
 
 
 @lru_cache()
-def get_covalent_distance_matrix(mol: Chem.Mol) -> np.array:
-    """
-    Generate a covalent matrix of the molecule.
+def get_covalent_distance_matrix(mol: Chem.Mol) -> npt.NDArray[np.float64]:
+    """Generate a covalent matrix of the molecule.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule to generate the derived covalent matrix of.
+        mol (Chem.Mol): The molecule to generate the derived covalent matrix of.
 
     Returns:
-        numpy.ndarray: A square covalent matrix of the molecule, where the value of each element is the covalent
-                       radius of the atoms.
+        npt.NDArray[np.float64]: A square covalent matrix of the molecule, where the value of each element is the covalent
+            radius of the atoms.
     """
-    covalent_radii = np.array(
+    covalent_radii = np.array([
         [
-            [
-                get_covalent_radius(mol.GetAtomWithIdx(i).GetAtomicNum())
-                for i in range(mol.GetNumAtoms())
-            ]
+            get_covalent_radius(mol.GetAtomWithIdx(i).GetAtomicNum())
+            for i in range(mol.GetNumAtoms())
         ]
-    )
+    ])
     return _create_matrix_with_radii_values(covalent_radii)
 
 
 @lru_cache()
-def get_bond_distance_matrix(mol: Chem.Mol) -> np.ndarray:
-    """
-    Get the bond distance matrix of the molecule.
+def get_bond_distance_matrix(mol: Chem.Mol) -> npt.NDArray[np.float64]:
+    """Get the bond distance matrix of the molecule.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule to get the bond distance matrix of.
+        mol (Chem.Mol): The molecule to get the bond distance matrix of.
 
     Returns:
-        numpy.ndarray: A square bond distance matrix of the molecule, where the value of each element is the
-                        bond distance between the atoms.
+        npt.NDArray[np.float64]: A square bond distance matrix of the molecule, where the value of each element is the
+            bond distance between the atoms.
     """
-    bond_radii = np.array(
+    bond_radii = np.array([
         [
-            [
-                get_bond_radius(mol.GetAtomWithIdx(i).GetAtomicNum())
-                for i in range(mol.GetNumAtoms())
-            ]
+            get_bond_radius(mol.GetAtomWithIdx(i).GetAtomicNum())
+            for i in range(mol.GetNumAtoms())
         ]
-    )
+    ])
     return _create_matrix_with_radii_values(bond_radii)
 
 
@@ -137,24 +134,26 @@ def _get_close_atoms(
     mol: Chem.Mol,
     conf_id: int = -1,
     threshold: float = 0.4,
-    reference: str = "vdw",
-) -> np.array:
-    """
-    Get a upper triangular matrix indicating if the distance between two atoms <= threshold * reference distance are too close
-    for non-bonding atoms.
+    reference: Literal["vdw", "covalent", "bond"] = "vdw",
+) -> npt.NDArray[np.int_]:
+    """Get a upper triangular matrix indicating close atoms.
+
+    If the distance between two atoms <= threshold * reference distance, the atoms are
+    considered to be too close. The reference distance is based on the van der Waals
+    radius, covalent radius or bond radius.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule.
+        mol (Chem.Mol): The molecule.
         conf_id (int, optional): The conformer ID of the molecule to get the distance matrix of. Defaults to ``-1``.
-        threshold (float): A multiplier applied on the reference matrix . Defaults to ``0.4``.
-        reference (str): The reference matrix to use. Defaults to ``"vdw"``.
-                         Options:
-                         - ``"vdw"`` for reference distance based on van de Waals radius
-                         - ``"covalent"`` for reference distance based on covalent radius
-                         - ``"bond"`` for reference distance based on bond radius
+        threshold (float, optional): A multiplier applied on the reference matrix . Defaults to ``0.4``.
+        reference (Literal["vdw", "covalent", "bond"], optional): The reference matrix to use. Defaults to ``"vdw"``.
+            Options:
+            - ``"vdw"`` for reference distance based on van de Waals radius
+            - ``"covalent"`` for reference distance based on covalent radius
+            - ``"bond"`` for reference distance based on bond radius
 
     Returns:
-        np.array: A upper triangular matrix indicating if the distance between two atoms <= threshold * reference distance are too close
+        npt.NDArray[np.int_]: A upper triangular matrix indicating if the distance between two atoms <= threshold * reference distance are too close
     """
     # Filter out the bonded atoms, as they are expected to be within the threshold
     adjacency_mat = get_adjacency_matrix(mol)
@@ -169,49 +168,53 @@ def has_colliding_atoms(
     mol: Chem.Mol,
     conf_id: int = -1,
     threshold: float = 0.4,
-    reference: str = "vdw",
+    reference: Literal["vdw", "covalent", "bond"] = "vdw",
 ) -> bool:
-    """
-    Check whether the molecule has colliding atoms. If the distance between two atoms <= threshold * reference distance,
+    """Check whether the molecule has colliding atoms.
+
+    If the distance between two atoms <= threshold * reference distance,
     the atoms are considered to be colliding.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule to check for colliding atoms.
+        mol (Chem.Mol): The molecule to check for colliding atoms.
         conf_id (int, optional): The conformer ID of the molecule to get the distance matrix of. Defaults to ``-1``.
-        threshold (float): A multiplier applied on the reference matrix . Defaults to ``0.4``.
-        reference (str): The reference matrix to use. Defaults to ``"vdw"``.
-                         Options:
-                         - ``"vdw"`` for reference distance based on van de Waals radius
-                         - ``"covalent"`` for reference distance based on covalent radius
-                         - ``"bond"`` for reference distance based on bond radius
-
-    Returns:
-        bool: Whether the molecule has colliding atoms.
-    """
-    return np.any(_get_close_atoms(mol, conf_id, threshold, reference))
-
-
-def get_colliding_atoms(
-    mol: Chem.Mol,
-    conf_id: int = -1,
-    threshold: float = 0.4,
-    reference: str = "vdw",
-) -> List[Tuple[int, int]]:
-    """
-    Get the atom pairs that are potentially colliding (if the distance between two atoms <= threshold * reference distance).
-
-    Args:
-        mol (rdkit.Chem.Mol): The molecule to check for colliding atoms.
-        conf_id (int, optional): The conformer ID of the molecule to get the distance matrix of. Defaults to ``-1``.
-        threshold (float): A multiplier applied on the reference matrix . Defaults to ``0.4``.
-        reference (str): The reference matrix to use. Defaults to ``"vdw"``.
+        threshold (float, optional): A multiplier applied on the reference matrix . Defaults to ``0.4``.
+        reference (Literal["vdw", "covalent", "bond"], optional): The reference matrix to use. Defaults to ``"vdw"``.
             Options:
             - ``"vdw"`` for reference distance based on van de Waals radius
             - ``"covalent"`` for reference distance based on covalent radius
             - ``"bond"`` for reference distance based on bond radius
 
     Returns:
-        list: A list of tuples of the atom indices that are potentially colliding.
+        bool: Whether the molecule has colliding atoms.
+    """
+    return bool(np.any(_get_close_atoms(mol, conf_id, threshold, reference)))
+
+
+def get_colliding_atoms(
+    mol: Chem.Mol,
+    conf_id: int = -1,
+    threshold: float = 0.4,
+    reference: Literal["vdw", "covalent", "bond"] = "vdw",
+) -> list[tuple[int, int]]:
+    """Get the atom pairs that are considered colliding.
+
+    The atoms are considered to be colliding if the distance between two atoms
+    <= threshold * reference distance. The reference distance is based on the
+    van der Waals radius, covalent radius or bond radius.
+
+    Args:
+        mol (Chem.Mol): The molecule to check for colliding atoms.
+        conf_id (int, optional): The conformer ID of the molecule to get the distance matrix of. Defaults to ``-1``.
+        threshold (float, optional): A multiplier applied on the reference matrix . Defaults to ``0.4``.
+        reference (Literal["vdw", "covalent", "bond"], optional): The reference matrix to use. Defaults to ``"vdw"``.
+            Options:
+            - ``"vdw"`` for reference distance based on van de Waals radius
+            - ``"covalent"`` for reference distance based on covalent radius
+            - ``"bond"`` for reference distance based on bond radius
+
+    Returns:
+        list[tuple[int, int]]: A list of tuples of the atom indices that are potentially colliding.
     """
     return list(zip(*np.nonzero(_get_close_atoms(mol, conf_id, threshold, reference))))
 
@@ -220,35 +223,48 @@ def get_missing_bonds(
     mol: Chem.Mol,
     conf_id: int = -1,
     threshold: float = 1.5,
-    reference: str = "covalent",
-) -> List[Tuple[int, int]]:
-    """
-    Check whether the molecule has missing bonds. If the distance between two atoms <= threshold * reference distance,
-    the bond between the atoms are considered to be missing.
+    reference: Literal["vdw", "covalent", "bond"] = "covalent",
+) -> list[tuple[int, int]]:
+    """Check whether the molecule has missing bonds heuristically.
+
+    If the distance between two atoms <= threshold * reference distance, the bond
+    between the atoms are considered to be missing.
 
     Args:
-        mol (rdkit.Chem.Mol): The molecule to check for missing bonds.
+        mol (Chem.Mol): The molecule to check for missing bonds.
         conf_id (int, optional): The conformer ID of the molecule to get the distance matrix of. Defaults to ``-1``.
-        threshold (float): A multiplier applied on the reference matrix . Defaults to ``1.5``.
-        reference (str): The reference matrix to use. Defaults to ``"covalent"``.
+        threshold (float, optional): A multiplier applied on the reference matrix . Defaults to ``1.5``.
+        reference (Literal["vdw", "covalent", "bond"], optional): The reference matrix to use. Defaults to ``"covalent"``.
             Options:
             - ``"vdw"`` for reference distance based on van de Waals radius
             - ``"covalent"`` for reference distance based on covalent radius
             - ``"bond"`` for reference distance based on bond radius
 
     Returns:
-        list: A list of tuples of the atom indices that are potentially missing bonds.
+        list[tuple[int, int]]: A list of tuples of the atom indices that are potentially missing bonds.
     """
     return get_colliding_atoms(mol, conf_id, threshold, reference)
 
 
-def _find_shortest_path(start, end, path=None, path_idxs=None):
-    """
-    Get the shortest path between two atoms in a molecule.
+def _find_shortest_path(
+    start: Chem.Atom,
+    end: Chem.Atom,
+    path: list[Chem.Atom] | None = None,
+    path_idxs: list[int] | None = None,
+) -> list[Chem.Atom] | None:
+    """Get the shortest path between two atoms in a molecule.
 
+    Args:
+        start (Chem.Atom): The starting atom.
+        end (Chem.Atom): The ending atom.
+        path (list[Chem.Atom] | None, optional): The current path. Defaults to None.
+        path_idxs (list[int] | None, optional): The current path indexes. Defaults to None.
+
+    Returns:
+        list[Chem.Atom] | None: A list of atoms in the shortest path between the two atoms.
     """
     path = path if path else []
-    path_idxs = path_idxs if path else []
+    path_idxs = path_idxs if path_idxs else []
     path = path + [start]
     path_idx = path_idxs + [start.GetIdx()]
     if path_idx[-1] == end.GetIdx():
@@ -264,31 +280,31 @@ def _find_shortest_path(start, end, path=None, path_idxs=None):
     return shortest
 
 
-def get_shortest_path(mol, idx1, idx2):
-    """
-    Get the shortest path between two atoms in a molecule. The RDKit GetShortestPath
-    has a very long setup time ~ 0.5ms (on test machine) regardless of the size of the molecule.
-    As a comparison, on the same machine, a naive python implementation of DFS (`_find_shortest_path`)
-    takes ~0.5 ms for a 100-C normal alkane end to end. Therefore, it make more sense to use a method
-    with a shorter setup time though scaling worse for smaller molecules while using GetShortestPath
-    for larger molecules.
+def get_shortest_path(mol: Chem.Mol, idx1: int, idx2: int) -> tuple[int, ...]:
+    """Get the shortest path between two atoms in a molecule.
+
+    The RDKit ``GetShortestPath``
+    has a very long setup time ~ 0.5ms (on test machine) regardless of the size of the
+    molecule. As a comparison, on the same machine, a naive python implementation of DFS
+    (`_find_shortest_path`) takes ~0.5 ms for a 100-C normal alkane end to end.
+    Therefore, it make more sense to use a method with a shorter setup time though
+    scaling worse for smaller molecules while using GetShortestPath for larger
+    molecules.
 
     Args:
-        mol (RDKitMol, RWMol): The molecule to be checked.
+        mol (Chem.Mol): The molecule to be checked.
         idx1 (int): The index of the first atom.
         idx2 (int): The index of the second atom.
 
     Returns:
-        list: A list of atoms in the shortest path between the two atoms.
+        tuple[int, ...]: A list of atoms in the shortest path between the two atoms.
     """
     if mol.GetNumHeavyAtoms() > 100:  # An empirical cutoff
         return Chem.GetShortestPath(mol, idx1, idx2)
 
-    shortest_path = _find_shortest_path(mol.GetAtomWithIdx(idx1), mol.GetAtomWithIdx(idx2))
+    shortest_path = _find_shortest_path(
+        mol.GetAtomWithIdx(idx1), mol.GetAtomWithIdx(idx2)
+    )
     if shortest_path is not None:
-        return tuple(
-            atom.GetIdx()
-            for atom in
-            _find_shortest_path(mol.GetAtomWithIdx(idx1), mol.GetAtomWithIdx(idx2))
-        )
+        return tuple(atom.GetIdx() for atom in shortest_path)
     return ()
