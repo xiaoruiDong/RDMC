13646583401c78d2eaa53a8551ac8fea1a8b53d4
Xiaorui Dong <dxr1234@gmail.com>
2025-04-07 00:56:24 -0400

    style: fix rdtools.conf

--- rdtools/conf.py
+++ rdtools/conf.py
@@ -1,37 +1,44 @@
-from typing import List, Optional, Union
+# -*- coding: utf-8 -*-
+"""A module contains functions to manipulate conformers in a molecule."""
+
+from typing import Any
 
 import numpy as np
+import numpy.typing as npt
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit.Chem import rdMolTransforms as rdMT
 
-from rdtools.bond import get_bonds_as_tuples, add_bonds
+from rdtools.bond import add_bonds, get_bonds_as_tuples
 
 
 def add_conformer(
     mol: Chem.RWMol,
-    conf: Optional[Chem.Conformer] = None,
-    coords: Optional[np.ndarray] = None,
-    conf_id: Optional[int] = None,
-):
-    """
-    Add a conformer to the current `RDKitMol`.
+    conf: Chem.Conformer | None = None,
+    coords: npt.NDArray[np.float64] | None = None,
+    conf_id: int | None = None,
+) -> int:
+    """Add a conformer to the current `RDKitMol`.
 
     Args:
         mol (Chem.RWMol): The molecule to be set.
-        conf (Conformer, optional): A user may provide a predefined ``conformer`` and add it to the ``mol``. Defaults to ``None``,
-                                    but either ``conf`` or ``coords`` must be provided.
-        coords (np.ndarray, optional): Instead of feeding a conformer, a user may provide the coordinates of the conformer to be added.
-                                       Defaults to ``None``, but either ``conf`` or ``coords`` must be provided.
-        conf_id (int, optional): Which ID to set for the conformer (will be added as the last conformer by default). Defaults to ``None``.
+        conf (Chem.Conformer | None, optional): A user may provide a predefined ``conformer`` and add it to the ``mol``. Defaults to ``None``,
+            but either ``conf`` or ``coords`` must be provided.
+        coords (npt.NDArray[np.float64] | None, optional): Instead of feeding a conformer, a user may provide the coordinates of the conformer to be added.
+            Defaults to ``None``, but either ``conf`` or ``coords`` must be provided.
+        conf_id (int | None, optional): Which ID to set for the conformer (will be added as the last conformer by default). Defaults to ``None``.
 
     Returns:
-        int: The conformer ID added
+        int: The conformer ID added.
+
+    Raises:
+        ValueError: If both ``conf`` and ``coords`` are ``None``.
     """
     assign_id = True if conf_id is None else False
     if conf is None:
-        try:
+        if coords is not None:
             conf = create_conformer(coords, conf_id)
-        except IndexError:  # Pass coords as None causing IndexError
+        else:
             raise ValueError("conf and coords cannot be both None.")
     elif conf_id is not None:
         conf.SetId(conf_id)
@@ -40,16 +47,21 @@ def add_conformer(
 
 def add_null_conformer(
     mol: Chem.RWMol,
-    conf_id: Optional[int] = None,
+    conf_id: int | None = None,
     random: bool = True,
-):
-    """
-    Embed a conformer with atoms' coordinates of random numbers or with all atoms
-    located at the origin to the current `RDKitMol`.
+) -> int:
+    """Embed a conformer with null atom coordinates.
+
+    The coordinates are either set to random numbers or all zero coordinates.
 
     Args:
         mol (Chem.RWMol): The molecule to be set.
-        conf_id (int, optional): Which ID to set for the conformer (will be added as the last conformer by default).
+        conf_id (int | None, optional): Which ID to set for the conformer (will be added as the last conformer by default).
+        random (bool, optional): Whether set coordinates to random numbers. Otherwise, set to all-zero
+            coordinates. Defaults to ``True``.
+
+    Returns:
+        int: The conformer ID added.
     """
     num_atoms = mol.GetNumAtoms()
     # Constructing a conformer with number of atoms will create a null conformer
@@ -60,15 +72,14 @@ def add_null_conformer(
 
 
 def create_conformer(
-    coords: np.ndarray,
-    conf_id: Optional[int] = None,
-):
-    """
-    Create a conformer with the given coordinates.
+    coords: npt.NDArray[np.float64],
+    conf_id: int | None = None,
+) -> Chem.Conformer:
+    """Create a conformer with the given coordinates.
 
     Args:
-        coords (np.ndarray): The coordinates to be set.
-        conf_id (int, optional): Which ID to set for the conformer. Defaults to ``0``.
+        coords (npt.NDArray[np.float64]): The coordinates to be set.
+        conf_id (int | None, optional): Which ID to set for the conformer. Defaults to ``0``.
 
     Returns:
         Chem.Conformer: The conformer created.
@@ -84,19 +95,21 @@ def embed_multiple_null_confs(
     mol: Chem.Mol,
     n: int = 10,
     random: bool = False,
-):
-    """
-    Embed conformers with null or random atom coordinates. This helps the cases where a conformer
-    can not be successfully embedded. You can choose to generate all zero coordinates or random coordinates.
-    You can set to all-zero coordinates, if you will set coordinates later; You should set to random
-    coordinates, if you want to optimize this molecule by force fields (RDKit force field cannot optimize
-    all-zero coordinates).
+) -> None:
+    """Embed conformers with null or random atom coordinates.
+
+    This helps the cases where
+    a conformer can not be successfully embedded. You can choose to generate all zero
+    coordinates or random coordinates. You can set to all-zero coordinates, if you will
+    set coordinates later; You should set to random coordinates, if you want to optimize
+    this molecule by force fields (RDKit force field cannot optimize all-zero
+    coordinates).
 
     Args:
         mol (Chem.Mol): The molecule to be set.
-        n (int): The number of conformers to be embedded. Defaults to ``10``.
+        n (int, optional): The number of conformers to be embedded. Defaults to ``10``.
         random (bool, optional): Whether set coordinates to random numbers. Otherwise, set to all-zero
-                                 coordinates. Defaults to ``True``.
+            coordinates. Defaults to ``True``.
     """
     mol.RemoveAllConformers()
     for i in range(n):
@@ -105,9 +118,8 @@ def embed_multiple_null_confs(
 
 def reflect(
     conf: Chem.Conformer,
-):
-    """
-    Reflect the coordinates of the conformer.
+) -> None:
+    """Reflect the coordinates of the conformer.
 
     Args:
         conf (Chem.Conformer): The conformer to be set.
@@ -118,17 +130,18 @@ def reflect(
 
 
 def set_conformer_coordinates(
-    conf: Chem.Conformer, coords: Union[tuple, list, np.ndarray]
-):
-    """
-    Set the Positions of atoms of the conformer.
+    conf: Chem.Conformer,
+    coords: (
+        tuple[tuple[float, float, float], ...]
+        | list[list[float]]
+        | npt.NDArray[np.float64]
+    ),
+) -> None:
+    """Set the Positions of atoms of the conformer.
 
     Args:
         conf (Chem.Conformer): The conformer to be set.
-        coords (Union[tuple, list, np.ndarray]): The coordinates to be set.
-
-    Raises:
-        ValueError: Not a valid ``coords`` input, when giving something else.
+        coords (tuple[tuple[float, float, float], ...] | list[list[float]] | npt.NDArray[np.float64]): The coordinates to be set.
     """
     if isinstance(coords, (tuple, list)):
         coords = np.array(coords)
@@ -140,26 +153,32 @@ def set_conformer_coordinates(
 def embed_conformer(
     mol: Chem.Mol,
     allow_null: bool = True,
-    **kwargs,
-):
-    """
-    Embed a conformer to the molecule object. This will overwrite current conformers. By default, it
-    will first try embedding a 3D conformer; if fails, it then try to compute 2D coordinates
-    and use that for the conformer structure; if both approaches fail, and embedding a null
-    conformer is allowed, a conformer with all random coordinates will be embedded. The last one is
-    helpful for the case where you can use `SetPositions` to set their positions afterward, or if you want to
-    optimize the geometry using force fields.
+    **kwargs: Any,
+) -> None:
+    """Embed a conformer to the molecule object.
+
+    This will overwrite current conformers.
+    By default, it will first try embedding a 3D conformer; if fails, it then try to
+    compute 2D coordinates and use that for the conformer structure; if both approaches
+    fail, and embedding a null conformer is allowed, a conformer with all random
+    coordinates will be embedded. The last one is helpful for the case where you can use
+    `SetPositions` to set their positions afterward, or if you want to optimize the
+    geometry using force fields.
 
     Args:
-        mol (Mol): The molecule object to embed.
+        mol (Chem.Mol): The molecule object to embed.
         allow_null (bool, optional): If embedding 3D and 2D coordinates fails, whether to embed a conformer
-                                     with all null coordinates, ``(0, 0, 0)``, for each atom. Defaults to ``True``.
+            with all null coordinates, ``(0, 0, 0)``, for each atom. Defaults to ``True``.
+        **kwargs (Any): The keyword arguments to pass to the embedding function.
+
+    Raises:
+        RuntimeError: If embedding fails and ``allow_null`` is ``False``.
     """
     try:
-        return_code = Chem.AllChem.EmbedMolecule(mol, **kwargs)
+        return_code = AllChem.EmbedMolecule(mol, **kwargs)  # type: ignore[attr-defined]
     except Chem.AtomValenceException:
         try:
-            Chem.AllChem.Compute2DCoords(mol)
+            AllChem.Compute2DCoords(mol)  # type: ignore[attr-defined]
             return_code = 0
         except BaseException:  # To be replaced by a more specific error type
             return_code = -1
@@ -171,21 +190,33 @@ def embed_conformer(
             raise RuntimeError("Cannot embed conformer for this molecule.")
 
 
-def embed_multiple_confs(mol: Chem.Mol, n: int = 10, allow_null: bool = True, **kwargs):
-    """
-    Embed multiple conformers to the ``RDKitMol``. This will overwrite current conformers. By default, it
-    will first try embedding a 3D conformer; if fails, it then try to compute 2D coordinates
-    and use that for the conformer structure; if both approaches fail, and embedding a null
-    conformer is allowed, a conformer with all random coordinates will be embedded. The last one is
-    helpful for the case where you can use `SetPositions` to set their positions afterward, or if you want to
-    optimize the geometry using force fields.
+def embed_multiple_confs(
+    mol: Chem.Mol,
+    n: int = 10,
+    allow_null: bool = True,
+    **kwargs: Any,
+) -> None:
+    """Embed multiple conformers to the ``RDKitMol``.
+
+    This will overwrite current
+    conformers. By default, it will first try embedding a 3D conformer; if fails, it
+    then try to compute 2D coordinates and use that for the conformer structure; if both
+    approaches fail, and embedding a null conformer is allowed, a conformer with all
+    random coordinates will be embedded. The last one is helpful for the case where you
+    can use `SetPositions` to set their positions afterward, or if you want to optimize
+    the geometry using force fields.
 
     Args:
-        n (int): The number of conformers to be embedded. The default is ``1``.
-        allow_null (bool): If embedding fails, whether to embed null conformers. Defaults to ``True``.
+        mol (Chem.Mol): The molecule object to embed.
+        n (int, optional): The number of conformers to be embedded. The default is ``1``.
+        allow_null (bool, optional): If embedding fails, whether to embed null conformers. Defaults to ``True``.
+        **kwargs (Any): The keyword arguments to pass to the embedding function.
+
+    Raises:
+        RuntimeError: If embedding fails and ``allow_null`` is ``False``.
     """
     try:
-        Chem.AllChem.EmbedMultipleConfs(mol, numConfs=n, **kwargs)
+        AllChem.EmbedMultipleConfs(mol, numConfs=n, **kwargs)  # type: ignore[attr-defined]
     except Chem.AtomValenceException:
         if allow_null:
             embed_multiple_null_confs(mol, n=n, random=True)
@@ -196,27 +227,30 @@ def embed_multiple_confs(mol: Chem.Mol, n: int = 10, allow_null: bool = True, **
 def edit_conf_by_add_bonds(
     conf: Chem.Conformer,
     function_name: str,
-    atoms: List[int],
-    value: float,
-):
-    """
-    RDKit forbids modifying internal coordinates with non-bonding atoms.
-    This function tries to provide a workaround.
+    atoms: tuple[int, ...],
+    value: int | float,
+) -> None:
+    """Add bonds to the conformer and modify the internal coordinates.
+
+    This is a workaround for RDKit forbidding modifying internal coordinates with
+    non-bonding atoms. The function will add the bonds, modify the internal coordinates,
+    and then remove the bonds. The conformer will be modified in place.
+    Note, the function will not work for ring systems.
 
     Args:
-        conf (Conformer): The conformer to be modified.
+        conf (Chem.Conformer): The conformer to be modified.
         function_name (str): The function name of the edit, should be a method provided in rdMolTransforms.
-        atoms (list): A list of atoms representing the internal coordinates.
-        value (float): Value to be set.
+        atoms (tuple[int, ...]): A list of atoms representing the internal coordinates.
+        value (int | float): Value to be set.
     """
     parent_mol = conf.GetOwningMol()
     all_bonds = get_bonds_as_tuples(parent_mol)
     bonds_to_add = [
         (atoms[i], atoms[i + 1])
         for i in range(len(atoms) - 1)
-        if not (atoms[i], atoms[i + 1]) in all_bonds
+        if (atoms[i], atoms[i + 1]) not in all_bonds
     ]
-    tmp_mol = add_bonds(tmp_mol, bonds_to_add, inplace=False)
+    tmp_mol = add_bonds(parent_mol, bonds_to_add, inplace=False)
 
     add_conformer(tmp_mol, conf.GetPositions(), conf_id=0)
     tmp_conf = tmp_mol.GetConformer()
@@ -227,14 +261,15 @@ def edit_conf_by_add_bonds(
 
 def get_bond_length(
     conf: Chem.Conformer,
-    atom_ids: List[int],
+    atom_ids: tuple[int, int],
 ) -> float:
-    """
-    Get the distance between two atoms. Although it is called get bond length, the two atoms can be non-bonded.
+    """Get the distance between two atoms.
+
+    Note, the two atoms can be non-bonded.
 
     Args:
-        conf (Conformer): The conformer to be set.
-        atom_ids (List[int]): The atom IDs to be set.
+        conf (Chem. Conformer): The conformer to be set.
+        atom_ids (tuple[int, int]): The atom IDs to be set.
 
     Returns:
         float: The bond length between two atoms.
@@ -244,14 +279,13 @@ def get_bond_length(
 
 def get_angle_deg(
     conf: Chem.Conformer,
-    atom_ids: List[int],
+    atom_ids: tuple[int, int, int],
 ) -> float:
-    """
-    Get the angle between three atoms in degrees.
+    """Get the angle between three atoms in degrees.
 
     Args:
-        conf (Conformer): The conformer to be set.
-        atom_ids (List[int]): The atom IDs to be set.
+        conf (Chem.Conformer): The conformer to be set.
+        atom_ids (tuple[int, int, int]): The atom IDs to be set.
 
     Returns:
         float: The angle between three atoms in degrees.
@@ -261,14 +295,13 @@ def get_angle_deg(
 
 def get_torsion_deg(
     conf: Chem.Conformer,
-    atom_ids: List[int],
+    atom_ids: tuple[int, int, int, int],
 ) -> float:
-    """
-    Get the torsion angle between four atoms in degrees.
+    """Get the torsion angle between four atoms in degrees.
 
     Args:
-        conf (Conformer): The conformer to be set.
-        atom_ids (List[int]): The atom IDs to be set.
+        conf (Chem.Conformer): The conformer to be set.
+        atom_ids (tuple[int, int, int, int]): The atom IDs to be set.
 
     Returns:
         float: The torsion angle between four atoms in degrees.
@@ -278,19 +311,23 @@ def get_torsion_deg(
 
 def set_bond_length(
     conf: Chem.Conformer,
-    atom_ids: List[int],
-    value: float,
-):
-    """
-    Set the distance between two atoms. If the two atoms are not bonded, the function
-    will give it a try by forming the bonds, adjusting the bond length, and then removing
-    the bonds. This is a workaround for RDKit forbidding modifying internal coordinates
-    with non-bonding atoms.
+    atom_ids: tuple[int, int],
+    value: int | float,
+) -> None:
+    """Set the distance between two atoms.
+
+    If the two atoms are not bonded, the function
+    will give it a try by forming the bonds, adjusting the bond length, and then
+    removing the bonds. This is a workaround for RDKit forbidding modifying internal
+    coordinates with non-bonding atoms.
 
     Args:
-        conf (Conformer): The conformer to be set.
-        atom_ids (List[int]): The atom IDs to be set.
-        value (float): The value to be set.
+        conf (Chem.Conformer): The conformer to be set.
+        atom_ids (tuple[int, int]): The atom IDs to be set.
+        value (int | float): The value to be set.
+
+    Raises:
+        NotImplementedError: If the approach for modifying the bond length is not available.
     """
     try:
         rdMT.SetBondLength(conf, *atom_ids, value)
@@ -307,19 +344,23 @@ def set_bond_length(
 
 def set_angle_deg(
     conf: Chem.Conformer,
-    atom_ids: List[int],
-    value: float,
-):
-    """
-    Set the angle between three atoms in degrees. If the three atoms are not bonded, the function
-    will give it a try by forming the bonds, adjusting the angle, and then removing
-    the bonds. This is a workaround for RDKit forbidding modifying internal coordinates
-    with non-bonding atoms.
+    atom_ids: tuple[int, int, int],
+    value: int | float,
+) -> None:
+    """Set the angle between three atoms in degrees.
+
+    If the three atoms are not bonded,
+    the function will give it a try by forming the bonds, adjusting the angle, and then
+    removing the bonds. This is a workaround for RDKit forbidding modifying internal
+    coordinates with non-bonding atoms.
 
     Args:
-        conf (Conformer): The conformer to be set.
-        atom_ids (List[int]): The atom IDs to be set.
-        value (float): The value to be set.
+        conf (Chem.Conformer): The conformer to be set.
+        atom_ids (tuple[int, int, int]): The atom IDs to be set.
+        value (int | float): The value to be set.
+
+    Raises:
+        NotImplementedError: If the approach for modifying the angle is not available.
     """
     try:
         rdMT.SetAngleDeg(conf, *atom_ids, value)
@@ -336,19 +377,23 @@ def set_angle_deg(
 
 def set_torsion_deg(
     conf: Chem.Conformer,
-    atom_ids: List[int],
-    value: float,
-):
-    """
-    Set the torsion angle between four atoms in degrees. If the four atoms are not bonded, the function
-    will give it a try by forming the bonds, adjusting the torsion, and then removing
-    the bonds. This is a workaround for RDKit forbidding modifying internal coordinates
-    with non-bonding atoms.
+    atom_ids: tuple[int, int, int, int],
+    value: int | float,
+) -> None:
+    """Set the torsion angle between four atoms in degrees.
+
+    If the four atoms are not
+    bonded, the function will give it a try by forming the bonds, adjusting the torsion,
+    and then removing the bonds. This is a workaround for RDKit forbidding modifying
+    internal coordinates with non-bonding atoms.
 
     Args:
-        conf (Conformer): The conformer to be set.
-        atom_ids (List[int]): The atom IDs to be set.
-        value (float): The value to be set.
+        conf (Chem.Conformer): The conformer to be set.
+        atom_ids (tuple[int, int, int, int]): The atom IDs to be set.
+        value (int | float): The value to be set.
+
+    Raises:
+        NotImplementedError: If the approach for modifying the torsion is not available.
     """
     try:
         rdMT.SetDihedralDeg(conf, *atom_ids, value)
