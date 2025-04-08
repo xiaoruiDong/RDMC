86481a8e5fc40a17505f35593d3e6230398ea22d
Xiaorui Dong <dxr1234@gmail.com>
2025-04-08 02:11:54 -0400

    style: fix rdtools.fix continued

--- rdtools/fix/__init__.py
+++ rdtools/fix/__init__.py
@@ -1,9 +1,9 @@
+"""Fixes for common issues in molecular structures."""
+
+from rdtools.fix.fix import fix_mol, fix_oxonium_bonds, remedy_manager
 from rdtools.fix.mult import (
     saturate_biradical_12,
     saturate_biradical_cdb,
     saturate_carbene,
     saturate_mol,
-)
-from rdtools.fix.fix import (
-    fix_mol, fix_oxonium_bonds, remedy_manager
 )
